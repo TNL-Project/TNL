ebb983c61228662a13574f844a1ef371c3f093f0
hayeuyur <hayeuyur@fit.cvut.cz>
2022-03-19 14:58:11 +0100

    Implement tests for combinations function

---
+++ src/UnitTests/Meshes/GridTests/Utils/GridTemplatesTest.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifdef HAVE_GTEST
+
+#include <gtest/gtest.h>
+
+#include <TNL/Containers/StaticVector.h>
+#include <TNL/Meshes/Templates.h>
+
+void testCombination(const int k, const int n, const int expectation) {
+   EXPECT_EQ(TNL::Meshes::Templates::combination(k, n), expectation) << k << " " << n;
+}
+
+TEST(BasisTestSuite, CombinationsTest) {
+  testCombination(0, 1, 1);
+  testCombination(1, 1, 1);
+
+  testCombination(0, 2, 1);
+  testCombination(1, 2, 2);
+  testCombination(2, 2, 1);
+
+  testCombination(0, 3, 1);
+  testCombination(1, 3, 3);
+  testCombination(2, 3, 3);
+  testCombination(3, 3, 1);
+
+  testCombination(0, 4, 1);
+  testCombination(1, 4, 4);
+  testCombination(2, 4, 6);
+  testCombination(3, 4, 4);
+  testCombination(4, 4, 1);
+}
+
+
+#endif
