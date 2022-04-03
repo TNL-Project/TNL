156fae2a6c32be00328e27893b91b0a9a50b2d74
hayeuyur <hayeuyur@fit.cvut.cz>
2022-04-03 11:20:48 +0200

    Move kernel launching in kernel definition

--- src/Benchmarks/Convolution/kernels/naive.h
+++ src/Benchmarks/Convolution/kernels/naive.h
@@ -3,39 +3,20 @@
 
 #ifdef HAVE_CUDA
 
-#include <TNL/Devices/Cuda.h>
-#include <TNL/Containers/StaticVector.h>
-#include <TNL/Cuda/LaunchHelpers.h>
+   #include <TNL/Devices/Cuda.h>
+   #include <TNL/Containers/StaticVector.h>
+   #include <TNL/Cuda/LaunchHelpers.h>
 
 /**
  * There are several pitfalls with such configuration.
  *
  * 1. At first we don't use shared memory
- * 2. At second we don't control block size, so we may launch extremely small kernels or otherwise we can launch extremely large kernels.
+ * 2. At second we don't control block size, so we may launch extremely small kernels or otherwise we can launch extremely large
+ * kernels.
  */
 
 template< int Dimension, typename Device >
 struct Convolution;
-
-template<>
-struct Convolution< 1, TNL::Devices::Cuda >
-{
-public:
-   template< typename Index >
-   using Vector = TNL::Containers::StaticVector< 1, Index >;
-
-   template< typename Index, typename Real >
-   static void
-   setup( TNL::Cuda::LaunchConfiguration& configuration, const Vector< Index >& dimensions, const Vector< Index >& kernelSize )
-   {
-      configuration.dynamicSharedMemorySize = 0;
-
-      // TODO: - Benchmark the best value
-      configuration.blockSize.x = kernelSize.x();
-      configuration.gridSize.x =
-         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.x(), configuration.blockSize.x ) );
-   }
-};
 
 template< typename Index,
           typename Real,
@@ -77,29 +58,6 @@ convolution1D( Index kernelWidth,
 
    store( ix, result );
 }
-
-template<>
-struct Convolution< 2, TNL::Devices::Cuda >
-{
-public:
-   template< typename Index >
-   using Vector = TNL::Containers::StaticVector< 2, Index >;
-
-   template< typename Index, typename Real >
-   static void
-   setup( TNL::Cuda::LaunchConfiguration& configuration, const Vector< Index >& dimensions, const Vector< Index >& kernelSize )
-   {
-      configuration.dynamicSharedMemorySize = 0;
-
-      configuration.blockSize.x = kernelSize.x();
-      configuration.blockSize.y = kernelSize.y();
-
-      configuration.gridSize.x =
-         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.x(), configuration.blockSize.x ) );
-      configuration.gridSize.y =
-         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.y(), configuration.blockSize.y ) );
-   }
-};
 
 template< typename Index,
           typename Real,
@@ -151,33 +109,6 @@ convolution2D( Index kernelWidth,
 
    store( ix, iy, result );
 }
-
-template<>
-struct Convolution< 3, TNL::Devices::Cuda >
-{
-public:
-   template< typename Index >
-   using Vector = TNL::Containers::StaticVector< 3, Index >;
-
-   template< typename Index, typename Real >
-   static void
-   setup( TNL::Cuda::LaunchConfiguration& configuration, const Vector< Index >& dimensions, const Vector< Index >& kernelSize )
-   {
-      configuration.dynamicSharedMemorySize = 0;
-
-      // TODO: - Benchmark the best value
-      configuration.blockSize.x = kernelSize.x();
-      configuration.blockSize.y = kernelSize.y();
-      configuration.blockSize.z = kernelSize.z();
-
-      configuration.gridSize.x =
-         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.x(), configuration.blockSize.x ) );
-      configuration.gridSize.y =
-         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.y(), configuration.blockSize.y ) );
-      configuration.gridSize.y =
-         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.z(), configuration.blockSize.z ) );
-   }
-};
 
 template< typename Index,
           typename Real,
@@ -243,5 +174,178 @@ convolution3D( Index kernelWidth,
 
    store( ix, iy, iz, result );
 }
+
+template< int Dimension, typename Device >
+struct Convolution;
+
+template<>
+struct Convolution< 1, TNL::Devices::Cuda >
+{
+public:
+   template< typename Index >
+   using Vector = TNL::Containers::StaticVector< 1, Index >;
+
+   template< typename Index, typename Real >
+   static void
+   setup( TNL::Cuda::LaunchConfiguration& configuration, const Vector< Index >& dimensions, const Vector< Index >& kernelSize )
+   {
+      configuration.dynamicSharedMemorySize = 0;
+
+      // TODO: - Benchmark the best value
+      configuration.blockSize.x = kernelSize.x();
+      configuration.gridSize.x =
+         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.x(), configuration.blockSize.x ) );
+   }
+
+   template< typename Index,
+             typename Real,
+             typename FetchData,
+             typename FetchBoundary,
+             typename FetchKernel,
+             typename Convolve,
+             typename Store >
+   static void
+   execute( const Vector< Index >& dimensions,
+            const Vector< Index >& kernelSize,
+            FetchData&& fetchData,
+            FetchBoundary&& fetchBoundary,
+            FetchKernel&& fetchKernel,
+            Convolve&& convolve,
+            Store&& store )
+   {
+      TNL::Cuda::LaunchConfiguration configuration;
+
+      setup< Index, Real >( configuration, dimensions, kernelSize );
+
+      constexpr auto kernel = convolution1D< Index, Real, FetchData, FetchBoundary, FetchKernel, Convolve, Store >;
+
+      TNL::Cuda::launchKernel< true >(
+         kernel, 0, configuration, kernelSize.x(), dimensions.x(), fetchData, fetchBoundary, fetchKernel, convolve, store );
+   };
+};
+
+template<>
+struct Convolution< 2, TNL::Devices::Cuda >
+{
+public:
+   template< typename Index >
+   using Vector = TNL::Containers::StaticVector< 2, Index >;
+
+   template< typename Index, typename Real >
+   static void
+   setup( TNL::Cuda::LaunchConfiguration& configuration, const Vector< Index >& dimensions, const Vector< Index >& kernelSize )
+   {
+      configuration.dynamicSharedMemorySize = 0;
+
+      configuration.blockSize.x = kernelSize.x();
+      configuration.blockSize.y = kernelSize.y();
+
+      configuration.gridSize.x =
+         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.x(), configuration.blockSize.x ) );
+      configuration.gridSize.y =
+         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.y(), configuration.blockSize.y ) );
+   }
+
+   template< typename Index,
+             typename Real,
+             typename FetchData,
+             typename FetchBoundary,
+             typename FetchKernel,
+             typename Convolve,
+             typename Store >
+   static void
+   execute( const Vector< Index >& dimensions,
+            const Vector< Index >& kernelSize,
+            FetchData&& fetchData,
+            FetchBoundary&& fetchBoundary,
+            FetchKernel&& fetchKernel,
+            Convolve&& convolve,
+            Store&& store )
+   {
+      TNL::Cuda::LaunchConfiguration configuration;
+
+      setup< Index, Real >( configuration, dimensions, kernelSize );
+
+      constexpr auto kernel = convolution2D< Index, Real, FetchData, FetchBoundary, FetchKernel, Convolve, Store >;
+
+      TNL::Cuda::launchKernel< true >( kernel,
+                                       0,
+                                       configuration,
+                                       kernelSize.x(),
+                                       kernelSize.y(),
+                                       dimensions.x(),
+                                       dimensions.y(),
+                                       fetchData,
+                                       fetchBoundary,
+                                       fetchKernel,
+                                       convolve,
+                                       store );
+   };
+};
+
+template<>
+struct Convolution< 3, TNL::Devices::Cuda >
+{
+public:
+   template< typename Index >
+   using Vector = TNL::Containers::StaticVector< 3, Index >;
+
+   template< typename Index, typename Real >
+   static void
+   setup( TNL::Cuda::LaunchConfiguration& configuration, const Vector< Index >& dimensions, const Vector< Index >& kernelSize )
+   {
+      configuration.dynamicSharedMemorySize = 0;
+
+      // TODO: - Benchmark the best value
+      configuration.blockSize.x = kernelSize.x();
+      configuration.blockSize.y = kernelSize.y();
+      configuration.blockSize.z = kernelSize.z();
+
+      configuration.gridSize.x =
+         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.x(), configuration.blockSize.x ) );
+      configuration.gridSize.y =
+         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.y(), configuration.blockSize.y ) );
+      configuration.gridSize.y =
+         TNL::min( TNL::Cuda::getMaxGridSize(), TNL::Cuda::getNumberOfBlocks( dimensions.z(), configuration.blockSize.z ) );
+   }
+
+   template< typename Index,
+             typename Real,
+             typename FetchData,
+             typename FetchBoundary,
+             typename FetchKernel,
+             typename Convolve,
+             typename Store >
+   static void
+   execute( const Vector< Index >& dimensions,
+            const Vector< Index >& kernelSize,
+            FetchData&& fetchData,
+            FetchBoundary&& fetchBoundary,
+            FetchKernel&& fetchKernel,
+            Convolve&& convolve,
+            Store&& store )
+   {
+      TNL::Cuda::LaunchConfiguration configuration;
+
+      setup< Index, Real >( configuration, dimensions, kernelSize );
+
+      constexpr auto kernel = convolution3D< Index, Real, FetchData, FetchBoundary, FetchKernel, Convolve, Store >;
+
+      TNL::Cuda::launchKernel< true >( kernel,
+                                       0,
+                                       configuration,
+                                       kernelSize.x(),
+                                       kernelSize.y(),
+                                       kernelSize.z(),
+                                       dimensions.x(),
+                                       dimensions.y(),
+                                       dimensions.z(),
+                                       fetchData,
+                                       fetchBoundary,
+                                       fetchKernel,
+                                       convolve,
+                                       store );
+   };
+};
 
 #endif
