3278101a1507201fc57d4c4d99082d6e0166ae45
Tomáš Oberhuber <oberhuber.tomas@gmail.com>
2022-04-14 22:29:21 +0200

    Moving grid gnuplot writrer.

--- src/TNL/Meshes/Writers/GnuplotWriter.h
+++ src/TNL/Meshes/Writers/GnuplotWriter.h
@@ -1,12 +1,8 @@
-/***************************************************************************
-                          GnuplotWriter.h  -  description
-                             -------------------
-    begin                : Jul 2, 2013
-    copyright            : (C) 2013 by Tomas Oberhuber
-    email                : tomas.oberhuber@fjfi.cvut.cz
- ***************************************************************************/
-
-/* See Copyright Notice in tnl/Copyright */
+// Copyright (c) 2004-2022 Tomáš Oberhuber et al.
+//
+// This file is part of TNL - Template Numerical Library (https://tnl-project.org/)
+//
+// SPDX-License-Identifier: MIT
 
 #pragma once
 
@@ -18,37 +14,35 @@ namespace Meshes {
 
 class GnuplotWriter
 {
-   public:
+public:
+   template< typename Element >
+   static void
+   write( std::ostream& str, const Element& d )
+   {
+      str << d;
+   }
 
-      template< typename Element >
-      static void write( std::ostream& str,
-                         const Element& d )
-      {
-         str << d;
-      }
+   template< typename Real >
+   static void
+   write( std::ostream& str, const Containers::StaticVector< 1, Real >& d )
+   {
+      str << d.x() << " ";
+   }
 
-      template< typename Real >
-      static void write( std::ostream& str,
-                         const Containers::StaticVector< 1, Real >& d )
-      {
-         str << d.x() << " ";
-      }
+   template< typename Real >
+   static void
+   write( std::ostream& str, const Containers::StaticVector< 2, Real >& d )
+   {
+      str << d.x() << " " << d.y() << " ";
+   }
 
-      template< typename Real >
-      static void write( std::ostream& str,
-                         const Containers::StaticVector< 2, Real >& d )
-      {
-         str << d.x() << " " << d.y() << " ";
-      }
-
-      template< typename Real >
-      static void write( std::ostream& str,
-                         const Containers::StaticVector< 3, Real >& d )
-      {
-         str << d.x() << " " << d.y() << " " << d. z() << " ";
-      }
-
+   template< typename Real >
+   static void
+   write( std::ostream& str, const Containers::StaticVector< 3, Real >& d )
+   {
+      str << d.x() << " " << d.y() << " " << d.z() << " ";
+   }
 };
 
-} // namespace Meshes
-} // namespace TNL
+}  // namespace Meshes
+}  // namespace TNL
