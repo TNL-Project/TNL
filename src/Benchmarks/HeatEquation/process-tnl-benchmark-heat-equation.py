8688b8ec3a4e3cb2282e3fac566950d520afda58
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2022-12-29 19:34:27 +0100

    Modifying scripts for running and processing results of the heat equation benchmark.

--- src/Benchmarks/HeatEquation/process-tnl-benchmark-heat-equation.py
+++ src/Benchmarks/HeatEquation/process-tnl-benchmark-heat-equation.py
@@ -9,19 +9,20 @@ import numpy as np
 import math
 from os.path import exists
 
-devices = [ "sequential", "host", 'cuda' ]
+dimensions = [ "1", "2", "3" ]
+devices = [ "sequential", "host", "cuda" ]
 precisions = [ "float", "double" ]
 tests = [
-    "parallel-for", "simple-grid", "grid", #"nd-grid",
+    "parallel-for", "nd-array", "grid"
 ]
 
 ####
 # Create multiindex for columns
 def get_multiindex():
-    level1 = [ 'xSize', 'ySize' ]
-    level2 = [ '',      ''      ]
-    level3 = [ '',      ''      ]
-    df_data = [[ ' ',' ']]
+    level1 = [ 'Dimension','xSize', 'ySize', 'zSize' ]
+    level2 = [ '',         '',      '',      ''      ]
+    level3 = [ '',         '',      '',      ''      ]
+    df_data = [[ '', '', '', '']]
     for test in tests:
         for device in devices:
             values = ['time']
@@ -41,7 +42,7 @@ def get_multiindex():
 
 ####
 # Process dataframe for given precision - float or double
-def processDf( df, precision ):
+def processDf( df, dimension, precision ):
     multicolumns, df_data = get_multiindex()
 
     frames = []
@@ -52,24 +53,29 @@ def processDf( df, precision ):
     x_sizes.sort()
     y_sizes = list(set(df['ySize']))
     y_sizes.sort()
+    z_sizes = list(set(df['zSize']))
+    z_sizes.sort()
 
     performers = []
 
     for x_size in x_sizes:
         for y_size in y_sizes:
-            aux_df=df.loc[ ( df['xSize'] == x_size ) & ( df['ySize'] == y_size ) ]
-            new_df = pd.DataFrame( df_data, columns = multicolumns, index = [out_idx] )
-            out_idx += 1
-            new_df.iloc[0][ ('xSize','','') ]  = x_size
-            new_df.iloc[0][ ('ySize','','') ]  = y_size
-            for index, row in aux_df.iterrows():
-                test = row[ 'implementation' ]
-                #print( test )
-                time = row[ 'time' ]
-                new_df.iloc[0][(test,row['performer'],'time') ] = float( time )
-                performers.append( row['performer'] )
-            #print( new_df )
-            frames.append( new_df)
+            for z_size in z_sizes:
+                aux_df=df.loc[ ( df['xSize'] == x_size ) & ( df['ySize'] == y_size )  & ( df['zSize'] == z_size ) ]
+                new_df = pd.DataFrame( df_data, columns = multicolumns, index = [out_idx] )
+                out_idx += 1
+                new_df.iloc[0][ ('dimension','','') ]  = dimension
+                new_df.iloc[0][ ('xSize','','') ]      = x_size
+                new_df.iloc[0][ ('ySize','','') ]      = y_size
+                new_df.iloc[0][ ('zSize','','') ]      = z_size
+                for index, row in aux_df.iterrows():
+                    test = row[ 'implementation' ]
+                    #print( test )
+                    time = row[ 'time' ]
+                    new_df.iloc[0][(test,row['performer'],'time') ] = float( time )
+                    performers.append( row['performer'] )
+                #print( new_df )
+                frames.append( new_df)
     result = pd.concat( frames )
     idx = 0
     have_cuda = ( performers.count( 'cuda' ) > 0 )
@@ -84,36 +90,37 @@ def processDf( df, precision ):
                     result.iloc[idx][ (test, device, 'parallel-for speed-up') ] =  float( row[ ('parallel-for', device, 'time')] ) / float( row[ (test, device, 'time')] )
         idx += 1
 
-    result.to_html( f'tnl-benchmark-heat-equation-{precision}.html' )
+    result.to_html( f'tnl-benchmark-heat-equation-{precision}-{dimension}d.html' )
 
 
 #####
 # Parse input files
 
 parsed_lines = []
-for device in devices:
-    for precision in precisions:
-        for test in tests:
-            filename = f"tnl-benchmark-heat-equation-{test}-{device}-{precision}.json"
-            if not exists( filename ):
-                print( f"Skipping non-existing input file {filename} ...." )
-                continue
-            print( f"Parsing input file {filename} ...." )
-            with open( filename ) as f:
-                lines = f.readlines()
-                for line in lines:
-                    parsed_line = json.loads(line)
-                    parsed_lines.append( parsed_line )
+for dimension in dimensions:
+    for device in devices:
+        for precision in precisions:
+            for test in tests:
+                filename = f"tnl-benchmark-heat-equation-{test}-{device}-{precision}-{dimension}d.json"
+                if not exists( filename ):
+                    print( f"Skipping non-existing input file {filename} ...." )
+                    continue
+                print( f"Parsing input file {filename} ...." )
+                with open( filename ) as f:
+                    lines = f.readlines()
+                    for line in lines:
+                        parsed_line = json.loads(line)
+                        parsed_lines.append( parsed_line )
 
 df = pd.DataFrame(parsed_lines)
 
-keys = ['xSize', 'ySize', 'zSize', 'time', 'bandwidth' ]
+keys = [ 'xSize', 'ySize', 'zSize', 'time', 'bandwidth' ]
 
 for key in keys:
     if key in df.keys():
         df[key] = pd.to_numeric(df[key])
-
-for precision in precisions:
-    aux_df = df.loc[ ( df['precision'] == precision ) ]
-    processDf( aux_df, precision )
+for dimension in dimensions:
+    for precision in precisions:
+        aux_df = df.loc[ ( df['precision'] == precision ) & ( df['dimension'] == dimension ) ]
+        processDf( aux_df, dimension, precision )
 
