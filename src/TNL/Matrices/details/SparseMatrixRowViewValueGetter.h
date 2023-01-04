83c48136b3de31ebee89a0f1b2fd9950936eb86a
Jakub Klinkovský <klinkovsky@mmg.fjfi.cvut.cz>
2023-01-04 15:19:33 +0100

    Bump copyright headers to 2023

--- src/TNL/Matrices/details/SparseMatrixRowViewValueGetter.h
+++ src/TNL/Matrices/details/SparseMatrixRowViewValueGetter.h
@@ -1,6 +1,6 @@
 
 
-// Copyright (c) 2004-2022 Tomáš Oberhuber et al.
+// Copyright (c) 2004-2023 Tomáš Oberhuber et al.
 //
 // This file is part of TNL - Template Numerical Library (https://tnl-project.org/)
 //
