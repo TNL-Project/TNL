a94790480a1bc2d4e4cb9a5400743894e46aaa6c
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2023-02-06 22:12:02 +0100

    Implementing script for processing results of memory access benchmark.

--- src/Benchmarks/System/process-tnl-benchmark-memory-access.py
+++ src/Benchmarks/System/process-tnl-benchmark-memory-access.py
@@ -10,32 +10,29 @@ import math
 from os.path import exists
 
 threads = [ ]
-access = [ "sequential", "random" ]
-tests = [
-    "parallel-for", "simple-grid", "grid", #"nd-grid",
-]
+accesses = [ "sequential", "random" ]
+element_sizes = [ 1, 2, 4, 8, 16, 32, 64, 128, 256 ]
 
 ####
 # Create multiindex for columns
 def get_multiindex():
-    level1 = [ 'xSize', 'ySize' ]
-    level2 = [ '',      ''      ]
-    level3 = [ '',      ''      ]
-    df_data = [[ ' ',' ']]
-    for test in tests:
-        for device in devices:
-            values = ['time']
-            if test != 'parallel-for':
-                values.append( 'parallel-for speed-up' )
-            if device == 'cuda':
-                values.append( 'CPU speed-up' )
-            for value in values:
-                level1.append( test )
-                level2.append( device )
-                level3.append( value )
-                df_data[0].append( '' )
+    level1 = [ 'size' ]
+    level2 = [ '',    ]
+    level3 = [ '',    ]
+    level4 = [ '',    ]
+    df_data = [[ ' ' ]]
+    for threads_count in threads:
+        for access in accesses:
+            for element_size in element_sizes:
+                values = ['time', "bandwidth", "CPU cycles"]
+                for value in values:
+                    level1.append( f'Threads {threads_count}' )
+                    level2.append( access )
+                    level3.append( element_size )
+                    level4.append( value )
+                    df_data[0].append( '' )
 
-    multiColumns = pd.MultiIndex.from_arrays([ level1, level2, level3 ] )
+    multiColumns = pd.MultiIndex.from_arrays([ level1, level2, level3, level4 ] )
     return multiColumns, df_data
 
 
@@ -48,48 +45,34 @@ def processDf( df ):
     in_idx = 0
     out_idx = 0
 
-    x_sizes = list(set(df['xSize']))
-    x_sizes.sort()
-    y_sizes = list(set(df['ySize']))
-    y_sizes.sort()
+    sizes = list(set(df['array size']))
 
-    performers = []
+    print( sizes )
 
-    for x_size in x_sizes:
-        for y_size in y_sizes:
-            aux_df=df.loc[ ( df['xSize'] == x_size ) & ( df['ySize'] == y_size ) ]
-            new_df = pd.DataFrame( df_data, columns = multicolumns, index = [out_idx] )
-            out_idx += 1
-            new_df.iloc[0][ ('xSize','','') ]  = x_size
-            new_df.iloc[0][ ('ySize','','') ]  = y_size
-            for index, row in aux_df.iterrows():
-                test = row[ 'implementation' ]
-                #print( test )
-                time = row[ 'time' ]
-                new_df.iloc[0][(test,row['performer'],'time') ] = float( time )
-                performers.append( row['performer'] )
-            #print( new_df )
-            frames.append( new_df)
+    for size in sizes:
+        aux_df=df.loc[ ( df['array size'] == size ) ]
+        #print( aux_df )
+        new_df = pd.DataFrame( df_data, columns = multicolumns, index = [out_idx] )
+        out_idx += 1
+        new_df.iloc[0][ ('size','','','') ]  = size
+        for index, row in aux_df.iterrows():
+            threads = row[ 'threads' ]
+            access_type = row[ 'access type' ]
+            element_size = row[ 'element size' ]
+            time = row[ 'time' ]
+            bandwidth = row[ 'bandwidth' ]
+            cpu_cycles = row[ 'cycles/op.' ]
+            print( f'Threads {threads} {access_type} {element_size} {time} {bandwidth} {cpu_cycles} ')
+            new_df.iloc[0][( f'Threads {threads}', access_type, element_size,'time') ] = time
+            new_df.iloc[0][( f'Threads {threads}', access_type, element_size,'bandwidth') ] = bandwidth
+            new_df.iloc[0][( f'Threads {threads}', access_type, element_size,'CPU cycles') ] = cpu_cycles
+        #print( new_df.iloc[0][( f'Threads {threads}', access_type, element_size,'time') ] )
+        frames.append( new_df)
     result = pd.concat( frames )
-    idx = 0
-    have_cuda = ( performers.count( 'cuda' ) > 0 )
-    for index, row in result.iterrows():
-        for test in tests:
-            if have_cuda:
-                result.iloc[idx][ (test, 'cuda', 'CPU speed-up') ] =  float( row[ (test, 'host', 'time')] ) / float( row[ (test, 'cuda', 'time')] )
-            if test != 'parallel-for':
-                for device in devices:
-                    if device == 'cuda' and not have_cuda:
-                        continue
-                    result.iloc[idx][ (test, device, 'parallel-for speed-up') ] =  float( row[ ('parallel-for', device, 'time')] ) / float( row[ (test, device, 'time')] )
-        idx += 1
-
     result.to_html( f'tnl-benchmark-memory-access.html' )
-
 
 #####
 # Parse input files
-
 parsed_lines = []
 filename = f"tnl-benchmark-memory-access.log"
 if not exists( filename ):
@@ -102,8 +85,11 @@ with open( filename ) as f:
         parsed_lines.append( parsed_line )
 
 df = pd.DataFrame(parsed_lines)
+for x in df['threads']:
+    if x not in threads:
+        threads.append( x )
 
-keys = ['time', 'bandwidth' ]
+keys = ['array size','time', 'bandwidth', 'cycles/op' ]
 
 for key in keys:
     if key in df.keys():
@@ -111,4 +97,3 @@ for key in keys:
 
 df.to_html( 'tnl-benchmark-memory-access.html' )
 processDf( df )
-
