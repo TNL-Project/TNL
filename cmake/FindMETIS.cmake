8842281d003f0593494521b84eebfcbe90fd51e7
Jakub Klinkovský <klinkovsky@mmg.fjfi.cvut.cz>
2023-03-22 15:15:28 +0100

    Copy-pasted more modern FindMETIS.cmake from Ginkgo to fix its build failure
    See https://github.com/ginkgo-project/ginkgo/issues/1308

--- cmake/FindMETIS.cmake
+++ cmake/FindMETIS.cmake
@@ -1,23 +1,67 @@
-# This module will try to find METIS and define the following:
-#  METIS_FOUND - True iff METIS was found
-#  METIS_INCLUDE_DIRS - METIS include directories
-#  METIS_LIBRARIES - METIS library paths
+#.rst:
+# FindMETIS
+# -------
+#
+# Find the METIS graph partitioning library.
+#
+# Imported targets
+# ^^^^^^^^^^^^^^^^
+#
+# This module defines the following :prop_tgt:`IMPORTED` target:
+#
+# ``METIS::METIS``
+#   The METIS library, if found.
+#
+# Result variables
+# ^^^^^^^^^^^^^^^^
+#
+# This module will set the following variables in your project:
+#
+# ``METIS_INCLUDE_DIRS``
+#   where to find metis.h or metis64.h
+#
+# ``METIS_HEADER``
+#   either metis64.h or metis.h, dependent on which was found
+#
+# ``METIS_LIBRARIES``
+#   the libraries to link against in order to use the METIS library.
+#
+# ``METIS_FOUND``
+#   If false, do not try to use the METIS library.
 
-find_path(METIS_INCLUDE_DIR metis.h
-          PATHS /usr/include /usr/local/include
-          DOC "METIS include directory")
+find_path(METIS_INCLUDE_DIR NAMES metis64.h metis.h HINTS ${METIS_DIR} ENV METIS_DIR PATH_SUFFIXES include)
 
-find_library(METIS_LIBRARY metis
-             PATHS /usr/lib /usr/local/lib ${PROJECT_SOURCE_DIR}/metis
-             DOC "Path to METIS library")
+if (METIS_INCLUDE_DIR)
+    if (EXISTS ${METIS_INCLUDE_DIR}/metis64.h)
+        set(METIS_HEADER metis64.h)
+        set(METIS_LIB_NAME metis64)
+    else()
+        set(METIS_HEADER metis.h)
+        set(METIS_LIB_NAME metis)
+    endif()
+    file(STRINGS ${METIS_INCLUDE_DIR}/${METIS_HEADER} metis_version_str_major REGEX "^#define[\t ]+METIS_VER_MAJOR[\t ]+.*")
+    file(STRINGS ${METIS_INCLUDE_DIR}/${METIS_HEADER} metis_version_str_minor REGEX "^#define[\t ]+METIS_VER_MINOR[\t ]+.*")
+    string(REGEX REPLACE "^#define[\t ]+METIS_VER_MAJOR[\t ]+([0-9]+).*" "\\1" METIS_VERSION_MAJOR "${metis_version_str_major}")
+    string(REGEX REPLACE "^#define[\t ]+METIS_VER_MINOR[\t ]+([0-9]+).*" "\\1" METIS_VERSION_MINOR "${metis_version_str_minor}")
+    set(METIS_VERSION ${METIS_VERSION_MAJOR}.${METIS_VERSION_MINOR})
+    find_library(METIS_LIBRARY ${METIS_LIB_NAME} HINTS ${METIS_DIR} ENV METIS_DIR PATH_SUFFIXES lib lib64)
+endif()
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set METIS_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(METIS  DEFAULT_MSG
-                                  METIS_LIBRARY METIS_INCLUDE_DIR)
+find_package_handle_standard_args(METIS REQUIRED_VARS METIS_LIBRARY METIS_INCLUDE_DIR VERSION_VAR METIS_VERSION)
 
-mark_as_advanced(METIS_INCLUDE_DIR METIS_LIBRARY)
+if(METIS_FOUND)
+    set(METIS_LIBRARIES ${METIS_LIBRARY})
+    set(METIS_INCLUDE_DIRS ${METIS_INCLUDE_DIR})
+    unset(METIS_LIBRARY)
+    unset(METIS_INCLUDE_DIR)
 
-set(METIS_LIBRARIES ${METIS_LIBRARY})
-set(METIS_INCLUDE_DIRS ${METIS_INCLUDE_DIR})
+    if(NOT TARGET METIS::METIS)
+        add_library(METIS::METIS UNKNOWN IMPORTED)
+        set_target_properties(METIS::METIS PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${METIS_INCLUDE_DIRS}")
+        set_target_properties(METIS::METIS PROPERTIES
+            IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+            IMPORTED_LOCATION "${METIS_LIBRARIES}")
+    endif()
+endif()
