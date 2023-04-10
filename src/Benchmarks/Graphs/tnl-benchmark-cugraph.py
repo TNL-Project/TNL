df758e37cad3fefc8415ac93fa3e3de3fa6f63f3
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2023-04-10 15:36:12 +0200

    Fixed benchmark with cuGraph.

--- src/Benchmarks/Graphs/tnl-benchmark-cugraph.py
+++ src/Benchmarks/Graphs/tnl-benchmark-cugraph.py
@@ -3,27 +3,29 @@ import argparse
 import cudf
 import cugraph
 
-# Read the input file and create a DataFrame
 def read_input_file(file_path):
-    df = cudf.read_csv(
-        file_path,
-        delimiter='\t',
-        skiprows=4,
-        names=['src', 'dst'],
-        dtype=['int32', 'int32']
-    )
+    data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            if line.startswith('#'):
+                continue  # Skip comments
+            src, dst = map(int, line.strip().split(' '))
+            data.append((src, dst))
+    df = cudf.DataFrame(data, columns=['src', 'dst'])
     return df
+
 
 # Create a cuGraph graph from the DataFrame
 def create_cugraph_graph(df):
-    G = cugraph.DiGraph()
+    G = cugraph.Graph(directed=True)
     G.from_cudf_edgelist(df, source='src', destination='dst')
     return G
 
 # Perform BFS and return a vector of distances from the source node
 def bfs_distances(G, source_vertex):
     bfs_df = cugraph.bfs(G, source_vertex)
-    distances = bfs_df['distance'].to_array()
+    bfs_df_pd = bfs_df.to_pandas()
+    distances = bfs_df_pd['distance'].to_numpy()
     return distances
 
 def main():
@@ -34,6 +36,7 @@ def main():
     # Read the input file and create a DataFrame
     print(f"Reading input file: {args.input_file}")
     df = read_input_file(args.input_file)
+    print( f'DataFrame = {df}' )
 
     # Create a cuGraph graph from the DataFrame
     G = create_cugraph_graph(df)
@@ -41,7 +44,8 @@ def main():
     # Perform BFS and get distances from the source vertex
     print("Performing BFS...")
     source_vertex = 0
-    distances = bfs_distances(G, source_vertex)
+
+    distances = bfs_distances(G, 0 ) #source_vertex)
 
     # Print the distances
     for i, distance in enumerate(distances):
