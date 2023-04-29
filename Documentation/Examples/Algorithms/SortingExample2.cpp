25fa5f3c1a17d15f3db3c47c05591da75ab0c30c
Jakub Klinkovský <klinkovsky@mmg.fjfi.cvut.cz>
2023-04-29 18:34:21 +0200

    Avoid calling a host-only function std::rand in a __cuda_callable__ lambda expression

--- Documentation/Examples/Algorithms/SortingExample2.cpp
+++ Documentation/Examples/Algorithms/SortingExample2.cpp
@@ -16,7 +16,9 @@ void sort( ArrayT& array )
    */
    Array< int > aux_array( size );
    srand( size + 2021 );
-   aux_array.forAllElements( [=] __cuda_callable__ ( int i, int& value ) { value = std::rand() % (2*size); } );
+   parallelFor< Devices::Host >( 0, size, [&]( int i ) {
+      aux_array[ i ] = std::rand() % (2*size);
+   });
    array = aux_array;
 
    std::cout << "Random array: " << array << std::endl;
