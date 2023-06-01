2c6d7dc5a7868b1e4989582bd72325a421afaa3d
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2023-06-01 15:07:28 +0200

    Added function Algorithms::find for finding an occurence of a given element in the array.

---
+++ src/TNL/Algorithms/find.h
@@ -0,0 +1,52 @@
+// Copyright (c) 2004-2023 Tomáš Oberhuber et al.
+//
+// This file is part of TNL - Template Numerical Library (https://tnl-project.org/)
+//
+// SPDX-License-Identifier: MIT
+
+// Implemented by: Tomas Oberhuber
+
+#pragma once
+
+#include <type_traits>
+#include <TNL/TypeTraits.h>
+#include <TNL/Algorithms/reduce.h>
+
+namespace TNL::Algorithms {
+
+// TODO: Add example to the documentation.
+/**
+ * \brief Find occurrence (not necessarily the first) of the value in the array.
+ *
+ * \tparam Container is the type of the container.
+ * \tparam ValueType is the type of the value to be found.
+ * \tparam IndexType is the type used for indexing.
+ * \param container is the array where the value is searched.
+ * \param value is the value to be found.
+ * \return pair `(found, position)` where \e found is a boolean indicating
+ *         if the \e value was found and \e position is the position of the
+ *         occurrence in the container.
+ */
+template< typename Container,
+          typename ValueType >
+std::pair< bool, typename Container::IndexType >
+find( const Container& container, const ValueType& value )
+{
+   static_assert( HasGetSizeMethod< Container >::value && HasSubscriptOperator< Container >::value );
+   using IndexType = typename Container::IndexType;
+   auto view = container.getConstView();
+   auto fetch = [ view, value ] __cuda_callable__( IndexType i ) -> bool
+   {
+      return view[ i ] == value;
+   };
+   auto reduce = [] __cuda_callable__( bool& a, bool b, IndexType& aIdx, IndexType bIdx )
+   {
+      if( b )
+         aIdx = bIdx;
+      a = ( a || b );
+   };
+   return Algorithms::reduceWithArgument< typename Container::DeviceType >(
+      (IndexType) 0, view.getSize(), fetch, reduce, false );
+}
+
+}  // namespace TNL::Algorithms
---
+++ src/UnitTests/Algorithms/findTest.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#ifdef HAVE_GTEST
+#include <vector>
+#include <TNL/Allocators/Host.h>
+#include <TNL/Allocators/Cuda.h>
+#include <TNL/Algorithms/find.h>
+#include <TNL/Containers/Array.h>
+
+#include "gtest/gtest.h"
+
+using namespace TNL;
+using namespace TNL::Algorithms;
+
+constexpr int ARRAY_TEST_SIZE = 5000;
+
+// test fixture for typed tests
+template< typename Array >
+class FindTest : public ::testing::Test
+{
+protected:
+   using ArrayType = Array;
+};
+
+// types for which ArrayTest is instantiated
+using ContainerTypes = ::testing::Types<
+   Containers::Array< short int, Devices::Sequential >,
+   Containers::Array< int, Devices::Sequential >,
+   Containers::Array< long int, Devices::Sequential >,
+   Containers::Array< float, Devices::Sequential >,
+   Containers::Array< double, Devices::Sequential >,
+   Containers::Array< short int, Devices::Host >,
+   Containers::Array< int, Devices::Host >,
+   Containers::Array< long int, Devices::Host >,
+   Containers::Array< float, Devices::Host >,
+   Containers::Array< double, Devices::Host >
+#ifdef __CUDA_CC__
+   ,Containers::Array< short int, Devices::Cuda >,
+   Containers::Array< int, Devices::Cuda >,
+   Containers::Array< long int, Devices::Cuda >,
+   Containers::Array< float, Devices::Cuda >,
+   Containers::Array< double, Devices::Cuda >
+#endif
+>;
+
+TYPED_TEST_SUITE( FindTest, ContainerTypes );
+
+TYPED_TEST( FindTest, find )
+{
+   using ArrayType = typename TestFixture::ArrayType;
+
+   ArrayType u{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+   EXPECT_EQ( find( u,  0 ).first, true );
+   EXPECT_EQ( find( u,  0 ).second, 0 );
+   EXPECT_EQ( find( u,  5 ).first, true );
+   EXPECT_EQ( find( u,  5 ).second, 5 );
+   EXPECT_EQ( find( u,  9 ).first, true );
+   EXPECT_EQ( find( u,  9 ).second, 9 );
+   EXPECT_EQ( find( u, 10 ).first, false );
+}
+
+#endif // HAVE_GTEST
+
+#include "../main.h"
