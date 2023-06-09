f98abfca21da962a0c3b3ef042215cb26ffdcb20
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2023-06-09 20:59:34 +0200

    Added GraphWriter.

---
+++ src/TNL/Algorithms/Graphs/GraphWriter.h
@@ -0,0 +1,52 @@
+// Copyright (c) 2004-2023 Tomáš Oberhuber et al.
+//
+// This file is part of TNL - Template Numerical Library (https://tnl-project.org/)
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+namespace TNL::Algorithms::Graphs {
+
+template< typename Graph >
+struct GraphWriter
+{
+   using GraphType = Graph;
+   using MatrixType = typename GraphType::MatrixType;
+   using ValueType = typename GraphType::ValueType;
+   using DeviceType = typename GraphType::DeviceType;
+   using IndexType = typename GraphType::IndexType;
+   using HostMatrixType = typename MatrixType::Self< ValueType, Devices::Host >;
+
+   static void writeEdgeList( std::ostream& str, const Graph& graph ) {
+      if constexpr( ! std::is_same< DeviceType, Devices::Host >::value &&
+         ! std::is_same< DeviceType, Devices::Host >::value ) {
+         HostMatrixType hostMatrix;
+         hostMatrix = graph.getAdjacencyMatrix();
+         writeEdgeList( str, hostMatrix );
+      }
+      else writeEdgeList( str, graph.getAdjacencyMatrix() );
+   }
+
+   static void writeEdgeList( const TNL::String& fileName, const Graph& graph ) {
+      std::ofstream file( fileName.getString() );
+      writeEdgeList( file, graph );
+   }
+
+   protected:
+
+   template< typename Matrix >
+   static void writeEdgeList( std::ostream& str, const Matrix& matrix ) {
+      for( IndexType rowIdx = 0; rowIdx < matrix.getRows(); rowIdx++ ) {
+         auto row = matrix.getRow( rowIdx );
+         for( IndexType localIdx = 0; localIdx < row.getSize(); localIdx++ ) {
+            auto columnIdx = row.getColumnIndex( localIdx );
+            if( columnIdx != matrix.getPaddingIndex() ) {
+               str << rowIdx << " " << columnIdx << " " << row.getValue( localIdx ) << std::endl;
+            }
+         }
+      }
+   }
+};
+
+} // namespace TNL::Algorithms::Graphs
