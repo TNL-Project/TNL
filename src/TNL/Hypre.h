d71c6e7de874fd0494b7f9a1bb8f3f274961fb1b
Jakub Klinkovský <klinkovsky@mmg.fjfi.cvut.cz>
2023-08-07 21:09:54 +0200

    Updated Hypre.h for HIP

--- src/TNL/Hypre.h
+++ src/TNL/Hypre.h
@@ -28,8 +28,8 @@
    #if defined( HYPRE_USING_CUDA ) && ! defined( __CUDACC__ )
       #error "__CUDACC__ is required when HYPRE is built with CUDA!"
    #endif
-   #if defined( HYPRE_USING_HIP ) && ! defined( HAVE_HIP )
-      #error "HAVE_HIP is required when HYPRE is built with HIP!"
+   #if defined( HYPRE_USING_HIP ) && ! defined( __HIP__ )
+      #error "__HIP__ is required when HYPRE is built with HIP!"
    #endif
 
 namespace TNL {
