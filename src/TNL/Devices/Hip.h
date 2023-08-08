91bdd558eb355ee25c082c87a927207437213d4a
Jakub Klinkovský <klinkovsky@mmg.fjfi.cvut.cz>
2023-08-08 18:56:49 +0200

    Renamed TNL::Devices::Cuda to TNL::Devices::GPU and added Hip and Cuda as type aliases

--- src/TNL/Devices/Hip.h
+++ src/TNL/Devices/Hip.h
@@ -16,6 +16,6 @@ namespace TNL::Devices {
  * It is not possible to build for multiple GPU backends at the same time, so
  * we can alias the types and avoid a huge amount of code duplication.
  */
-using Cuda = GPU;
+using Hip = GPU;
 
 }  // namespace TNL::Devices
