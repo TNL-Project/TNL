b34699128b3f2bd4a99fb49bc086d6a6e84e0afd
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2023-11-28 16:15:31 +0100

    Improved distributed array example.

--- Documentation/Examples/Containers/DistributedArrayExample.cpp
+++ Documentation/Examples/Containers/DistributedArrayExample.cpp
@@ -7,7 +7,8 @@
  * The following works for any device (CPU, GPU ...).
  */
 template< typename Device >
-void distributedArrayExample()
+void
+distributedArrayExample()
 {
    using ArrayType = TNL::Containers::DistributedArray< int, Device >;
    using IndexType = typename ArrayType::IndexType;
@@ -17,17 +18,39 @@ void distributedArrayExample()
 
    // We set the global array size to a prime number to force non-uniform distribution.
    const int size = 97;
-   const int ghosts = (communicator.size() > 1) ? 4 : 0;
+   const int ghosts = ( communicator.size() > 1 ) ? 4 : 0;
 
    const LocalRangeType localRange = TNL::Containers::splitRange< IndexType >( size, communicator );
    ArrayType a( localRange, ghosts, size, communicator );
-   a.forElements( 0, size, [] __cuda_callable__ ( int idx, int& value ) { value = idx; } );
-   std::cout << "Rank " << communicator.rank() << ": " << a.getLocalView() << std::endl;
+   a.forElements( 0,
+                  size,
+                  [] __cuda_callable__( int idx, int& value )
+                  {
+                     value = idx;
+                  } );
+   ArrayType b( localRange, ghosts, size, communicator );
+   b.forElements( 0,
+                  size,
+                  [] __cuda_callable__( int idx, int& value )
+                  {
+                     value = idx - ( idx == 90 );
+                  } );
+   for( int i = 0; i < communicator.size(); i++ ) {
+      if( communicator.rank() == i )
+         std::cout << "MPI rank = " << communicator.rank() << std::endl
+                   << " size = " << a.getSize() << std::endl
+                   << " local range = " << a.getLocalRange().getBegin() << " - " << a.getLocalRange().getEnd() << std::endl
+                   << " ghosts = " << a.getGhosts() << std::endl
+                   << " local data = " << a.getLocalView() << std::endl
+                   << " local data with ghosts = " << a.getLocalViewWithGhosts() << std::endl;
+      TNL::MPI::Barrier();
+   }
 }
 
-int main( int argc, char* argv[] )
+int
+main( int argc, char* argv[] )
 {
-   TNL::MPI::ScopedInitializer mpi(argc, argv);
+   TNL::MPI::ScopedInitializer mpi( argc, argv );
 
    if( TNL::MPI::GetRank() == 0 )
       std::cout << "The first test runs on CPU ..." << std::endl;
