d3629f1143a82b4b4151231cfc4b1e5f0dcac411
Jakub Klinkovský <klinkovsky@mmg.fjfi.cvut.cz>
2023-12-06 17:20:31 +0100

    Configure default CUDA GPU architecture in CMake
    CMAKE_CUDA_ARCHITECTURES=70 is not a good fallback, it does not work
    on Pascal GPUs...

---
+++ cmake/cuda_default_architecture.cmake
@@ -0,0 +1,25 @@
+# select one default GPU architecture to avoid building for all GPU architectures
+if( NOT DEFINED CMAKE_CUDA_ARCHITECTURES )
+   if( CMAKE_CUDA_COMPILER_ID STREQUAL "NVIDIA" )
+      set( CMAKE_CUDA_ARCHITECTURES "native" )
+   else()
+      # CMake's native GPU arch detection does not work with Clang
+      execute_process(
+         COMMAND
+         "nvidia-smi" "--query-gpu=compute_cap" "--format=csv,noheader"
+         RESULT_VARIABLE _status
+         OUTPUT_VARIABLE _stdout
+         ERROR_VARIABLE _stderr
+         OUTPUT_STRIP_TRAILING_WHITESPACE
+         ERROR_STRIP_TRAILING_WHITESPACE
+      )
+      if( ${_status} EQUAL 0 )
+         string( REPLACE "." "" _stdout ${_stdout} )
+         string( REGEX REPLACE "[ \t\r\n]+" ";" CMAKE_CUDA_ARCHITECTURES ${_stdout} )
+         list( REMOVE_DUPLICATES CMAKE_CUDA_ARCHITECTURES )
+      else()
+         message( WARNING "Failed to detect native GPU architecture using nvidia-smi: ${_stderr}" )
+      endif()
+   endif()
+   message( "-- Selected CMAKE_CUDA_ARCHITECTURES = ${CMAKE_CUDA_ARCHITECTURES}")
+endif()
