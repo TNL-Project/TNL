fc0c643134bf80a68522cec18780fbde9eef0194
Marek Salaba <markomio50@gmail.com>
2024-04-09 13:05:01 +0200

    Optimalization of the Shifted Power Iteration function

--- src/TNL/Matrices/Eigen/ShiftedPowerIteration.h
+++ src/TNL/Matrices/Eigen/ShiftedPowerIteration.h
@@ -11,36 +11,52 @@ namespace TNL::Matrices::Eigen {
 /**
  * \brief Calculates an eigenvalue and its corresponding eigenvector of a matrix using the shifted power iteration method.
  *
- * The shifted power iteration method is an adaptation of the standard power iteration algorithm. It accelerates the convergence
- * of the power iteration to a specific eigenvalue that is closer to the shift value. This method is particularly useful when
- * the largest eigenvalue does not converge quickly or when interest lies in an eigenvalue that is not the largest.
+ * The shifted power iteration method enhances the standard power iteration algorithm by targeting convergence towards an
+ * eigenvalue closer to a specified shift value. This is achieved by shifting the spectrum of the matrix, making a specific
+ * eigenvalue more dominant and thus accelerating convergence. This method is particularly advantageous when seeking an
+ * eigenvalue other than the largest, or when the largest eigenvalue converges slowly.
  *
- * \tparam T is the data type of the matrix elements (e.g., float, double), which determines the precision of the computations.
- * \tparam Device is the computational device where the data is stored and operations are performed (e.g., CPU, GPU).
- * \tparam MatrixType is the type of the matrix (e.g., dense matrix, sparse matrix).
+ * \tparam T Data type of the matrix elements (e.g., float, double), influencing the precision of computations.
+ * \tparam Device Computational device (e.g., CPU, GPU) for data storage and operations.
+ * \tparam MatrixType Type of matrix (e.g., dense, sparse) involved in the computation.
  *
- * \param matrix is the original matrix for which the eigenvalue and eigenvector are to be calculated.
- * \param epsilon is the precision of the calculation, which determines the convergence threshold for the iterative method.
- * \param shiftValue is the value used to shift the spectrum of the matrix. The choice of shiftValue influences which
- * eigenvalue the method will converge to.
+ * \param matrix Original matrix for which to calculate the eigenvalue and eigenvector.
+ * \param epsilon Precision threshold for convergence, determining when the iterative process should halt.
+ * \param shiftValue Shift value applied to the matrix spectrum, influencing the convergence target eigenvalue.
+ * \param initialVec (Overload) Initial vector for iteration, required to have the same dimension as the matrix.
+ * \param maxIterations (Optional) Maximum number of iterations to perform, defaults to 100000. Convergence failure within
+ *                      this limit results in returning the last computed values with an iteration count of -1.
  *
  * \return A tuple containing three elements:
- *         1. The eigenvalue (of type T) closest to the shiftValue.
- *         2. The corresponding eigenvector (of type TNL::Containers::Vector< T, Device >).
- *         3. The number of iterations (of type uint) that were performed to reach convergence.
+ *         - The eigenvalue closest to the shiftValue (of type T).
+ *         - The corresponding eigenvector (of type TNL::Containers::Vector<T, Device>).
+ *         - The number of iterations performed to reach convergence (of type int), where -1 indicates non-convergence
+ *           within the specified iteration limit.
  *
- * \note This method assumes that the input matrix is square and that a dominant eigenvalue exists close to the shift value.
- * \note The effectiveness of convergence depends on the choice of shiftValue. A value closer to the desired eigenvalue
- * generally leads to faster convergence.
+ * \exception std::invalid_argument Thrown if the input matrix is not square, if it is zero-sized, or if the initial vector
+ *                                  (in the overload that requires one) does not match the matrix's dimensions.
+ *
+ * \note The effectiveness of the shifted power iteration depends heavily on the choice of shiftValue; a shift closer to
+ *       the target eigenvalue can significantly enhance convergence speed. This method assumes the input matrix is square
+ *       and possesses a dominant eigenvalue near the shift value for successful convergence.
+ * \note In the overload without an initial vector, a random vector is generated as a starting point. The nature of this
+ *       initial vector can affect the algorithm's convergence speed and success.
  */
 template< typename T, typename Device, typename MatrixType >
-static std::tuple< T, TNL::Containers::Vector< T, Device >, uint >
-ShiftedPowerIterationTuple( const MatrixType& matrix, const T& epsilon, const T& shiftValue )
+static std::tuple< T, TNL::Containers::Vector< T, Device >, int >
+ShiftedPowerIteration( const MatrixType& matrix,
+                       const T& epsilon,
+                       const T& shiftValue,
+                       TNL::Containers::Vector< T, Device >& initialVec,
+                       const uint& maxIterations = 100000 )
 {
-   TNL_ASSERT_EQ( matrix.getRows(), matrix.getColumns(), "Shifted power iteration is possible only for square matrices" );
+   if( matrix.getRows() == matrix.getColumns() )
+      std::invalid_argument( "Shifted power iteration is possible only for square matrices" );
+   if( matrix.getRows() == 0 )
+      std::invalid_argument( "Zero-sized matrices are not allowed" );
    using IndexType = typename MatrixType::IndexType;
    int size = matrix.getColumns();
-   auto rowLengths = [ = ] __cuda_callable__( const IndexType rows, const IndexType  columns, const IndexType  rowIdx ) -> int
+   auto rowLengths = [ = ] __cuda_callable__( const IndexType rows, const IndexType columns, const IndexType rowIdx ) -> int
    {
       if( shiftValue != 0 && matrix.getElement( rowIdx, rowIdx ) == 0 ) {
          return matrix.getRowCapacity( rowIdx ) + 1;
@@ -49,8 +65,12 @@ ShiftedPowerIterationTuple( const MatrixType& matrix, const T& epsilon, const T&
          return matrix.getRowCapacity( rowIdx );
       }
    };
-   auto matrixElements = [ = ] __cuda_callable__(
-                            const IndexType  rows, const IndexType  columns, const IndexType  rowIdx, const IndexType  localIdx, IndexType & columnIdx, T& value )
+   auto matrixElements = [ = ] __cuda_callable__( const IndexType rows,
+                                                  const IndexType columns,
+                                                  const IndexType rowIdx,
+                                                  const IndexType localIdx,
+                                                  IndexType& columnIdx,
+                                                  T& value )
    {
       auto row = matrix.getRow( rowIdx );
       IndexType size = row.getSize();
@@ -66,44 +86,30 @@ ShiftedPowerIterationTuple( const MatrixType& matrix, const T& epsilon, const T&
             value = row.getValue( localIdx );
       }
    };
-   using MatrixFactory = TNL::Matrices::LambdaMatrixFactory< T, Device, int >;
+   using MatrixFactory = TNL::Matrices::LambdaMatrixFactory< T, Device, IndexType >;
    auto shiftedMatrix = MatrixFactory::create( size, size, matrixElements, rowLengths );
-   std::tuple< T, TNL::Containers::Vector< T, Device >, uint > tuple =
-      TNL::Matrices::Eigen::powerIterationTuple< T, Device >( shiftedMatrix, epsilon );
+   std::tuple< T, TNL::Containers::Vector< T, Device >, int > tuple =
+      TNL::Matrices::Eigen::powerIteration< T, Device >( shiftedMatrix, epsilon, std::move( initialVec ), maxIterations );
    return std::make_tuple( std::get< 0 >( tuple ) - shiftValue, std::get< 1 >( tuple ), std::get< 2 >( tuple ) );
 }
 
-/**
- * \brief Calculates an eigenvalue and its corresponding eigenvector of a matrix using the shifted power iteration method.
- *
- * The shifted power iteration method is an adaptation of the standard power iteration algorithm. It accelerates the convergence
- * of the power iteration to a specific eigenvalue that is closer to the shift value. This method is particularly useful when
- * the largest eigenvalue does not converge quickly or when interest lies in an eigenvalue that is not the largest.
- *
- * \tparam T is the data type of the matrix elements (e.g., float, double), which determines the precision of the computations.
- * \tparam Device is the computational device where the data is stored and operations are performed (e.g., CPU, GPU).
- * \tparam MatrixType is the type of the matrix (e.g., dense matrix, sparse matrix).
- *
- * \param matrix is the original matrix for which the eigenvalue and eigenvector are to be calculated.
- * \param epsilon is the precision of the calculation, which determines the convergence threshold for the iterative method.
- * \param shiftValue is the value used to shift the spectrum of the matrix. The choice of shiftValue influences which
- * eigenvalue the method will converge to.
- *
- * \return A pair consisting of:
- *         1. The eigenvalue (of type T) closest to the shiftValue.
- *         2. The corresponding eigenvector (of type TNL::Containers::Vector< T, Device >).
- *
- * \note This method assumes that the input matrix is square and that a dominant eigenvalue exists close to the shift value.
- * \note The effectiveness of convergence depends on the choice of shiftValue. A value closer to the desired eigenvalue
- * generally leads to faster convergence.
- */
 template< typename T, typename Device, typename MatrixType >
-static std::pair< T, TNL::Containers::Vector< T, Device > >
-ShiftedPowerIteration( const MatrixType& matrix, const T& epsilon, const T& shiftValue )
+static std::tuple< T, TNL::Containers::Vector< T, Device >, int >
+ShiftedPowerIteration( const MatrixType& matrix, const T& epsilon, const T& shiftValue, const uint& maxIterations = 100000 )
 {
-   std::tuple< T, TNL::Containers::Vector< T, Device >, uint > tuple =
-      ShiftedPowerIterationTuple< T, Device >( matrix, epsilon, shiftValue );
-   return std::make_pair( std::get< 0 >( tuple ), std::get< 1 >( tuple ) );
+   using IndexType = typename MatrixType::IndexType;
+   if( matrix.getRows() == 0 )
+      std::invalid_argument( "Zero-sized matrices are not allowed" );
+   IndexType vecSize = matrix.getRows();
+   TNL::Containers::Vector< T, Device > initialVec( vecSize );
+   initialVec.resize( vecSize );
+   if constexpr( std::is_integral_v< T > ) {
+      TNL::Algorithms::fillRandom< Device >( initialVec.getData(), vecSize, (T) -10000, (T) 10000 );
+   }
+   else {
+      TNL::Algorithms::fillRandom< Device >( initialVec.getData(), vecSize, (T) -1, (T) 1 );
+   }
+   return ShiftedPowerIteration( matrix, epsilon, shiftValue, std::move( initialVec ), maxIterations );
 }
 
 }  //namespace TNL::Matrices::Eigen
