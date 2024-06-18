80c44deb08611c1947e0c0dea17ceb0190dc32fb
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2024-06-18 15:38:37 +0200

    Added check for zeros on the diagonal of the matrix in the diagonal preconditioner.

--- src/Benchmarks/LinearSolvers/benchmarks.h
+++ src/Benchmarks/LinearSolvers/benchmarks.h
@@ -111,6 +111,10 @@ benchmarkSolver( Benchmark<>& benchmark,
    }
    catch( const std::runtime_error& ) {
    }
+   catch( std::invalid_argument& e ) {
+      std::cerr << e.what() << ". Skipping the benchmark!" << std::endl;
+      return;
+   }
 
    Vector x;
    x.setLike( x0 );
@@ -255,8 +259,10 @@ benchmarkArmadillo( const Config::ParameterContainer& parameters,
    arma::vec r = A * arma_x - arma_b;
    //    std::cout << "Converged: " << (time > 0) << ", residue = " << arma::norm( r ) / arma::norm( arma_b ) << " " <<
    //    std::endl; std::cout << "Mean time: " << time / loops << " seconds." << std::endl;
-   std::cout << "Converged: " << std::setw( 5 ) << std::boolalpha << ( time > 0 ) << "   " << "iterations = " << std::setw( 4 )
-             << "N/A" << "   " << "residue = " << std::setw( 10 ) << arma::norm( r ) / arma::norm( arma_b ) << "   "
+   std::cout << "Converged: " << std::setw( 5 ) << std::boolalpha << ( time > 0 ) << "   "
+             << "iterations = " << std::setw( 4 ) << "N/A"
+             << "   "
+             << "residue = " << std::setw( 10 ) << arma::norm( r ) / arma::norm( arma_b ) << "   "
              << "mean time = " << std::setw( 9 ) << time / loops << " seconds." << std::endl;
 }
 #endif
