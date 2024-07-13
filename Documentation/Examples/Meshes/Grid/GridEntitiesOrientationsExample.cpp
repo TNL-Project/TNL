3433b114a9457f3b9be0ed94ffab2f196582a76b
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2024-07-13 09:55:28 +0200

    Deleting useless type definition in GridEntitiesOrientationsExample.cpp.

--- Documentation/Examples/Meshes/Grid/GridEntitiesOrientationsExample.cpp
+++ Documentation/Examples/Meshes/Grid/GridEntitiesOrientationsExample.cpp
@@ -3,29 +3,34 @@
 #include <TNL/Meshes/GridEntitiesOrientations.h>
 
 template< int GridDimension >
-void writeGridEntitiesOrientations()
+void
+writeGridEntitiesOrientations()
 {
    using EntitiesOrientations = TNL::Meshes::GridEntitiesOrientations< GridDimension >;
-   using NormalsType = typename EntitiesOrientations::NormalsType;
 
-   std::cout << "Grid dimension      Entity dimension    Entity orientation index      Total orientation index      Normals    " << std::endl;
-   std::cout << "------------------------------------------------------------------------------------------------------------------" << std::endl;
+   std::cout << "Grid dimension      Entity dimension    Entity orientation index      Total orientation index      Normals    "
+             << std::endl;
+   std::cout
+      << "------------------------------------------------------------------------------------------------------------------"
+      << std::endl;
    EntitiesOrientations entitiesOrientations;
    for( int entityDimension = 0; entityDimension <= GridDimension; entityDimension++ )
-      for( int orientationIndex = 0; orientationIndex < EntitiesOrientations::getOrientationsCount( entityDimension ); orientationIndex++ )
+      for( int orientationIndex = 0; orientationIndex < EntitiesOrientations::getOrientationsCount( entityDimension );
+           orientationIndex++ )
       {
          int totalOrientationIndex = EntitiesOrientations::getTotalOrientationIndex( entityDimension, orientationIndex );
 
-         std::cout << std::setw( 10 ) << GridDimension
-                   << std::setw( 20 ) << entityDimension
-                   << std::setw( 25 ) << orientationIndex
-                   << std::setw( 25 ) << totalOrientationIndex
-                   << std::setw( 20 ) << entitiesOrientations.getNormals( totalOrientationIndex ) << std::endl;
+         std::cout << std::setw( 10 ) << GridDimension << std::setw( 20 ) << entityDimension << std::setw( 25 )
+                   << orientationIndex << std::setw( 25 ) << totalOrientationIndex << std::setw( 20 )
+                   << entitiesOrientations.getNormals( totalOrientationIndex ) << std::endl;
       }
-   std::cout << "------------------------------------------------------------------------------------------------------------------" << std::endl;
+   std::cout
+      << "------------------------------------------------------------------------------------------------------------------"
+      << std::endl;
 }
 
-int main( int argc, char* argv[] )
+int
+main( int argc, char* argv[] )
 {
    writeGridEntitiesOrientations< 1 >();
    writeGridEntitiesOrientations< 2 >();
