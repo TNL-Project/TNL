98c43b5916a732b62c4d4a7f2d1dea9c8ab414d0
Jakub Klinkovský <jakub.klinkovsky@fjfi.cvut.cz>
2024-08-05 17:38:23 +0200

    Remove separate TNL_CXX, TNL_CUDA, TNL_HIP interface targets

--- cmake/cuda_flags.cmake
+++ cmake/cuda_flags.cmake
@@ -1,7 +1,7 @@
 # set necessary CUDA compiler flags on the interface
 target_compile_options(
-    TNL_CUDA
-    INTERFACE $<$<CUDA_COMPILER_ID:NVIDIA>:
+    TNL
+    INTERFACE $<$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>:
               --expt-relaxed-constexpr
               ;
               --extended-lambda
@@ -16,8 +16,8 @@ target_compile_options(
 #   reference for the --diag_suppress and --display_error_number flags: https://stackoverflow.com/a/54142937
 #   incomplete list of tokens: http://www.ssl.berkeley.edu/~jimm/grizzly_docs/SSL/opt/intel/cc/9.0/lib/locale/en_US/mcpcom.msg
 target_compile_options(
-    TNL_CUDA
-    INTERFACE $<$<CUDA_COMPILER_ID:NVIDIA>:
+    TNL
+    INTERFACE $<$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>:
               -Wno-deprecated-gpu-targets
               ;
               "SHELL:-Xcudafe --diag_suppress=code_is_unreachable"
@@ -34,12 +34,12 @@ target_compile_options(
 )
 # This diagnostic is just plain wrong in CUDA 9 and later, see https://github.com/kokkos/kokkos/issues/1470
 target_compile_options(
-    TNL_CUDA INTERFACE $<$<CUDA_COMPILER_ID:NVIDIA>: "SHELL:-Xcudafe --diag_suppress=esa_on_defaulted_function_ignored" >
+    TNL INTERFACE $<$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>: "SHELL:-Xcudafe --diag_suppress=esa_on_defaulted_function_ignored" >
 )
 # nvcc 10 causes many invalid VLA errors in the host code
 target_compile_options(
-    TNL_CUDA INTERFACE $<$<AND:$<CUDA_COMPILER_ID:NVIDIA>,$<VERSION_LESS:$<CUDA_COMPILER_VERSION>,11>>:
-                       "SHELL:-Xcompiler -Wno-vla" >
+    TNL INTERFACE $<$<AND:$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>,$<VERSION_LESS:$<CUDA_COMPILER_VERSION>,11>>:
+                  "SHELL:-Xcompiler -Wno-vla" >
 )
 
 # set project-specific (i.e. not exported) build options
@@ -81,8 +81,8 @@ endif()
 
 # force colorized output (the automatic detection in compilers does not work with Ninja)
 target_compile_options(
-    TNL_CUDA
-    INTERFACE $<$<CUDA_COMPILER_ID:Clang>:-fcolor-diagnostics> ;
+    TNL
+    INTERFACE $<$<COMPILE_LANG_AND_ID:CUDA,Clang>:-fcolor-diagnostics> ;
               # nvcc does not support colored diagnostics
-              #$<$<CUDA_COMPILER_ID:NVIDIA>:> ;
+              #$<$<COMPILE_LANG_AND_ID:CUDA,NVIDIA>:> ;
 )
