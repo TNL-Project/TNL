e9b8790af2f8ed8c889e3ade43da7e9af38beb45
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2024-11-27 09:47:02 +0100

    Implementing new variants of forElements for segments.

--- src/TNL/Algorithms/Segments/CSRBase.hpp
+++ src/TNL/Algorithms/Segments/CSRBase.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <TNL/Algorithms/parallelFor.h>
+#include <TNL/Backend.h>
 
 #include "CSRBase.h"
 
@@ -128,29 +129,248 @@ CSRBase< Device, Index >::getOffsets() const -> ConstOffsetsView
    return this->offsets.getConstView();
 }
 
-template< typename Device, typename Index >
-template< typename Function >
+template< typename OffsetsView, typename Index, typename Function >
+__global__
 void
-CSRBase< Device, Index >::forElements( IndexType begin, IndexType end, Function&& function ) const
+forElementsKernel( Index gridIdx, OffsetsView offsets, Index begin, Index end, Function function )
 {
-   const auto offsetsView = this->offsets;
-   auto l = [ = ] __cuda_callable__( IndexType segmentIdx ) mutable
-   {
-      const IndexType begin = offsetsView[ segmentIdx ];
-      const IndexType end = offsetsView[ segmentIdx + 1 ];
-      IndexType localIdx( 0 );
-      for( IndexType globalIdx = begin; globalIdx < end; globalIdx++ )
-         function( segmentIdx, localIdx++, globalIdx );
-   };
-   Algorithms::parallelFor< Device >( begin, end, l );
+#if defined( __CUDACC__ ) || defined( __HIP__ )
+
+   // We map one warp to each segment
+   const Index segmentIdx = begin + Backend::getGlobalThreadIdx_x( gridIdx ) / Backend::getWarpSize();
+   if( segmentIdx >= end )
+      return;
+
+   //const Index laneIdx = threadIdx.x & ( Backend::getWarpSize() - 1 );  // & is cheaper than %
+   const Index laneIdx = threadIdx.x % Backend::getWarpSize();  // & is cheaper than %
+   TNL_ASSERT_LT( segmentIdx + 1, offsets.getSize(), "" );
+   Index endIdx = offsets[ segmentIdx + 1 ];
+
+   Index localIdx = laneIdx;
+   for( Index globalIdx = offsets[ segmentIdx ] + laneIdx; globalIdx < endIdx; globalIdx += Backend::getWarpSize() ) {
+      TNL_ASSERT_LT( globalIdx, endIdx, "" );
+      /*printf( ">>> threadIdx %d segmentIdx %d laneIdx %d localIdx %d globalIdx %d \n",
+              threadIdx.x,
+              segmentIdx,
+              laneIdx,
+              localIdx,
+              globalIdx );*/
+      function( segmentIdx, localIdx, globalIdx );
+      localIdx += Backend::getWarpSize();
+   }
+#endif
 }
 
 template< typename Device, typename Index >
 template< typename Function >
 void
-CSRBase< Device, Index >::forAllElements( Function&& function ) const
+CSRBase< Device, Index >::forElements( IndexType begin, IndexType end, Function function ) const
+{
+   if( end <= begin )
+      return;
+
+   if constexpr( std::is_same_v< Device, Devices::Cuda > || std::is_same_v< Device, Devices::Hip > ) {
+      const Index warpsCount = end - begin;
+      const std::size_t threadsCount = warpsCount * Backend::getWarpSize();
+      Backend::LaunchConfiguration launch_config;
+      launch_config.blockSize.x = 256;
+      dim3 blocksCount;
+      dim3 gridsCount;
+      Backend::setupThreads( launch_config.blockSize, blocksCount, gridsCount, threadsCount );
+      for( unsigned int gridIdx = 0; gridIdx < gridsCount.x; gridIdx++ ) {
+         Backend::setupGrid( blocksCount, gridsCount, gridIdx, launch_config.gridSize );
+         constexpr auto kernel = forElementsKernel< ConstOffsetsView, IndexType, Function >;
+         Backend::launchKernelAsync( kernel, launch_config, gridIdx, this->offsets, begin, end, function );
+      }
+      Backend::streamSynchronize( launch_config.stream );
+   }
+   else {
+      const auto offsetsView = this->offsets;
+      auto l = [ = ] __cuda_callable__( IndexType segmentIdx ) mutable
+      {
+         const IndexType begin = offsetsView[ segmentIdx ];
+         const IndexType end = offsetsView[ segmentIdx + 1 ];
+         IndexType localIdx( 0 );
+         for( IndexType globalIdx = begin; globalIdx < end; globalIdx++ )
+            function( segmentIdx, localIdx++, globalIdx );
+      };
+      Algorithms::parallelFor< Device >( begin, end, l );
+   }
+}
+
+template< typename Device, typename Index >
+template< typename Function >
+void
+CSRBase< Device, Index >::forAllElements( Function function ) const
 {
    this->forElements( 0, this->getSegmentsCount(), function );
+}
+
+template< typename OffsetsView, typename ArrayView, typename Index, typename Function >
+__global__
+void
+forElementsWithSegmentIndexesKernel( Index gridIdx,
+                                     OffsetsView offsets,
+                                     ArrayView segmentIndexes,
+                                     Index begin,
+                                     Index end,
+                                     Function function )
+{
+#if defined( __CUDACC__ ) || defined( __HIP__ )
+
+   // We map one warp to each segment
+   const Index idx = begin + Backend::getGlobalThreadIdx_x( gridIdx ) / Backend::getWarpSize();
+   if( idx >= end )
+      return;
+   TNL_ASSERT_GE( 0, segmentIndexes.getSize(), "" );
+   TNL_ASSERT_LT( idx, segmentIndexes.getSize(), "" );
+   const Index segmentIdx = segmentIndexes[ idx ];
+   TNL_ASSERT_GE( segmentIdx, 0, "Wrong index segment index - smaller that 0." );
+   TNL_ASSERT_LT( segmentIdx, offsets.getSize() - 1, "Wrong index segment index - larger that the number of indexes." );
+
+   const Index laneIdx = threadIdx.x & ( Backend::getWarpSize() - 1 );  // & is cheaper than %
+   //const Index laneIdx = threadIdx.x % Backend::getWarpSize();  // & is cheaper than %
+   TNL_ASSERT_LT( segmentIdx + 1, offsets.getSize(), "" );
+   Index endIdx = offsets[ segmentIdx + 1 ];
+
+   Index localIdx = laneIdx;
+   for( Index globalIdx = offsets[ segmentIdx ] + laneIdx; globalIdx < endIdx; globalIdx += Backend::getWarpSize() ) {
+      TNL_ASSERT_LT( globalIdx, endIdx, "" );
+      function( segmentIdx, localIdx, globalIdx );
+      localIdx += Backend::getWarpSize();
+   }
+#endif
+}
+
+template< typename Device, typename Index >
+template< typename Array, typename Function >
+void
+CSRBase< Device, Index >::forElements( const Array& segmentIndexes, Index begin, Index end, Function function ) const
+{
+   if( end <= begin )
+      return;
+   auto segmentIndexesView = segmentIndexes.getConstView();
+   if constexpr( std::is_same_v< Device, Devices::Cuda > || std::is_same_v< Device, Devices::Hip > ) {
+      const Index warpsCount = end - begin;
+      const std::size_t threadsCount = warpsCount * Backend::getWarpSize();
+      Backend::LaunchConfiguration launch_config;
+      launch_config.blockSize.x = 256;
+      dim3 blocksCount;
+      dim3 gridsCount;
+      Backend::setupThreads( launch_config.blockSize, blocksCount, gridsCount, threadsCount );
+      for( unsigned int gridIdx = 0; gridIdx < gridsCount.x; gridIdx++ ) {
+         Backend::setupGrid( blocksCount, gridsCount, gridIdx, launch_config.gridSize );
+         constexpr auto kernel =
+            forElementsWithSegmentIndexesKernel< ConstOffsetsView, typename Array::ConstViewType, IndexType, Function >;
+         Backend::launchKernelAsync( kernel, launch_config, gridIdx, this->offsets, segmentIndexesView, begin, end, function );
+      }
+      Backend::streamSynchronize( launch_config.stream );
+   }
+   else {
+      const auto offsetsView = this->offsets;
+      auto l = [ = ] __cuda_callable__( IndexType idx ) mutable
+      {
+         TNL_ASSERT_LT( idx, segmentIndexesView.getSize(), "" );
+         const IndexType segmentIdx = segmentIndexesView[ idx ];
+         TNL_ASSERT_GE( segmentIdx, 0, "Wrong index segment index - smaller that 0." );
+         TNL_ASSERT_LT(
+            segmentIdx, offsetsView.getSize() - 1, "Wrong index segment index - larger that the number of indexes." );
+         const IndexType begin = offsetsView[ segmentIdx ];
+         const IndexType end = offsetsView[ segmentIdx + 1 ];
+         IndexType localIdx( 0 );
+         for( IndexType globalIdx = begin; globalIdx < end; globalIdx++ ) {
+            TNL_ASSERT_LT( globalIdx, this->getStorageSize(), "" );
+            function( segmentIdx, localIdx++, globalIdx );
+         }
+      };
+      Algorithms::parallelFor< Device >( begin, end, l );
+   }
+}
+
+template< typename Device, typename Index >
+template< typename Array, typename Function >
+void
+CSRBase< Device, Index >::forElements( const Array& segmentIndexes, Function function ) const
+{
+   this->forElements( segmentIndexes, 0, segmentIndexes.getSize(), function );
+}
+
+template< typename OffsetsView, typename Index, typename Condition, typename Function >
+__global__
+void
+forElementsIfKernel( Index gridIdx, OffsetsView offsets, Index begin, Index end, Condition condition, Function function )
+{
+#if defined( __CUDACC__ ) || defined( __HIP__ )
+
+   // We map one warp to each segment
+   const Index segmentIdx = begin + Backend::getGlobalThreadIdx_x( gridIdx ) / Backend::getWarpSize();
+   if( segmentIdx >= end || ! condition( segmentIdx ) )
+      return;
+
+   //const Index laneIdx = threadIdx.x & ( Backend::getWarpSize() - 1 );  // & is cheaper than %
+   const Index laneIdx = threadIdx.x % Backend::getWarpSize();  // & is cheaper than %
+   TNL_ASSERT_LT( segmentIdx + 1, offsets.getSize(), "" );
+   Index endIdx = offsets[ segmentIdx + 1 ];
+
+   Index localIdx = laneIdx;
+   for( Index globalIdx = offsets[ segmentIdx ] + laneIdx; globalIdx < endIdx; globalIdx += Backend::getWarpSize() ) {
+      TNL_ASSERT_LT( globalIdx, endIdx, "" );
+      /*printf( ">>> threadIdx %d segmentIdx %d laneIdx %d localIdx %d globalIdx %d \n",
+              threadIdx.x,
+              segmentIdx,
+              laneIdx,
+              localIdx,
+              globalIdx );*/
+      function( segmentIdx, localIdx, globalIdx );
+      localIdx += Backend::getWarpSize();
+   }
+#endif
+}
+
+template< typename Device, typename Index >
+template< typename Condition, typename Function >
+void
+CSRBase< Device, Index >::forElementsIf( IndexType begin, IndexType end, Condition condition, Function function ) const
+{
+   if constexpr( std::is_same_v< Device, Devices::Cuda > || std::is_same_v< Device, Devices::Hip > ) {
+      if( end <= begin )
+         return;
+
+      const Index warpsCount = end - begin;
+      const std::size_t threadsCount = warpsCount * Backend::getWarpSize();
+      Backend::LaunchConfiguration launch_config;
+      launch_config.blockSize.x = 256;
+      dim3 blocksCount;
+      dim3 gridsCount;
+      Backend::setupThreads( launch_config.blockSize, blocksCount, gridsCount, threadsCount );
+      for( unsigned int gridIdx = 0; gridIdx < gridsCount.x; gridIdx++ ) {
+         Backend::setupGrid( blocksCount, gridsCount, gridIdx, launch_config.gridSize );
+         constexpr auto kernel = forElementsIfKernel< ConstOffsetsView, IndexType, Condition, Function >;
+         Backend::launchKernelAsync( kernel, launch_config, gridIdx, this->offsets, begin, end, condition, function );
+      }
+      Backend::streamSynchronize( launch_config.stream );
+   }
+   else {
+      const auto offsetsView = this->offsets;
+      auto l = [ = ] __cuda_callable__( IndexType segmentIdx ) mutable
+      {
+         const IndexType begin = offsetsView[ segmentIdx ];
+         const IndexType end = offsetsView[ segmentIdx + 1 ];
+         IndexType localIdx( 0 );
+         if( condition( segmentIdx ) )
+            for( IndexType globalIdx = begin; globalIdx < end; globalIdx++ )
+               function( segmentIdx, localIdx++, globalIdx );
+      };
+      Algorithms::parallelFor< Device >( begin, end, l );
+   }
+}
+
+template< typename Device, typename Index >
+template< typename Condition, typename Function >
+void
+CSRBase< Device, Index >::forAllElementsIf( Condition condition, Function function ) const
+{
+   this->forElementsIf( 0, this->getSegmentsCount(), condition, function );
 }
 
 template< typename Device, typename Index >
