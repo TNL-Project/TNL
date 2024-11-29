dabde06067491841a0aad6cc607a6bf310640c58
Jakub Klinkovský <jakub.klinkovsky@fjfi.cvut.cz>
2024-11-29 23:11:59 +0100

    CMake: Bump git tag for tinyxml2 to 10.0.0

--- cmake/FetchTinyXML2.cmake
+++ cmake/FetchTinyXML2.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
     tinyxml2
     GIT_REPOSITORY https://github.com/leethomason/tinyxml2.git
-    GIT_TAG 9.0.0
+    GIT_TAG 10.0.0
     FIND_PACKAGE_ARGS NAMES tinyxml2 SYSTEM
 )
 FetchContent_MakeAvailable(tinyxml2)
