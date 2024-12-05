1b2cea6023e44b704c28ea69126276c07d56f989
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2024-12-05 17:12:52 +0100

    Implementing more efficient BFS algorithm.

--- src/Benchmarks/Graphs/GraphsBenchmark.h
+++ src/Benchmarks/Graphs/GraphsBenchmark.h
@@ -110,11 +110,12 @@ struct GraphsBenchmark
             {
                x = x == std::numeric_limits< Index >::max() ? -1 : x;
             } );
-         this->boostBfsDistancesDirected.setSize(boost_bfs_dist.getSize());
-         for( Index i = 0; i < boost_bfs_dist.getSize(); i++)
+         this->boostBfsDistancesDirected.setSize( boost_bfs_dist.getSize() );
+         for( Index i = 0; i < boost_bfs_dist.getSize(); i++ )
             if( boost_bfs_dist[ i ] == 0 && i != largestNode )
                this->boostBfsDistancesDirected[ i ] = -1;
-            else this->boostBfsDistancesDirected[ i ] = boost_bfs_dist[ i ];
+            else
+               this->boostBfsDistancesDirected[ i ] = boost_bfs_dist[ i ];
 
          // Benchmarking breadth-first search of undirected graph
          benchmark.setMetadataElement( { "problem", "BFS undir" } );
@@ -131,12 +132,12 @@ struct GraphsBenchmark
             {
                x = x == std::numeric_limits< Index >::max() ? -1 : x;
             } );
-         this->boostBfsDistancesUndirected.setSize(boost_bfs_dist.getSize());
-         for( Index i = 0; i < boost_bfs_dist.getSize(); i++)
+         this->boostBfsDistancesUndirected.setSize( boost_bfs_dist.getSize() );
+         for( Index i = 0; i < boost_bfs_dist.getSize(); i++ )
             if( boost_bfs_dist[ i ] == 0 && i != largestNode )
                this->boostBfsDistancesUndirected[ i ] = -1;
-            else this->boostBfsDistancesUndirected[ i ] = boost_bfs_dist[ i ];
-
+            else
+               this->boostBfsDistancesUndirected[ i ] = boost_bfs_dist[ i ];
       }
 
       if( this->withSssp ) {
@@ -328,7 +329,7 @@ struct GraphsBenchmark
    #ifdef HAVE_BOOST
          if( this->boostSSSPDistancesDirected != this->gunrockSSSPDistancesDirected ) {
             std::cout << "SSSP distances of directed graph from Boost and Gunrock are not equal!" << std::endl;
-           this->errors++;
+            this->errors++;
          }
    #endif
 
@@ -401,9 +402,10 @@ struct GraphsBenchmark
          if( bfsDistances != this->boostBfsDistancesDirected ) {
             std::cout << "BFS distances of directed graph from Boost and TNL are not equal!" << std::endl;
             this->errors++;
-            for( Index i = 0; i < digraph.getNodeCount();i++)
-               if( bfsDistances[ i ] != this->boostBfsDistancesDirected[ i ])
-                  std::cerr << "i = " << i << " TNL -> " << bfsDistances[ i ] << " Boost -> " << this->boostBfsDistancesDirected[ i ] << std::endl;
+            //for( Index i = 0; i < digraph.getNodeCount(); i++ )
+            //   if( bfsDistances.getElement( i ) != this->boostBfsDistancesDirected[ i ] )
+            //      std::cerr << "i = " << i << " TNL -> " << bfsDistances.getElement( i ) << " Boost -> "
+            //                << this->boostBfsDistancesDirected[ i ] << std::endl;
          }
 #endif
 #ifdef HAVE_GUNROCK
@@ -427,6 +429,10 @@ struct GraphsBenchmark
          if( bfsDistances != this->boostBfsDistancesUndirected ) {
             std::cout << "BFS distances of undirected graph from Boost and TNL are not equal!" << std::endl;
             this->errors++;
+            //for( Index i = 0; i < digraph.getNodeCount(); i++ )
+            //   if( bfsDistances.getElement( i ) != this->boostBfsDistancesUndirected[ i ] )
+            //      std::cerr << "i = " << i << " TNL -> " << bfsDistances.getElement( i ) << " Boost -> "
+            //                << this->boostBfsDistancesUndirected[ i ] << std::endl;
          }
 #endif
 #ifdef HAVE_GUNROCK
@@ -435,7 +441,6 @@ struct GraphsBenchmark
             this->errors++;
          }
 #endif
-
       }
 
       if( this->withSssp ) {
