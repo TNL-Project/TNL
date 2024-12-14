36b6d34ee248ae88cb3932551147184bce72dca3
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2024-12-14 07:54:59 +0100

    Implementing CSR segments traversing as free functions.

---
+++ src/TNL/Algorithms/Segments/operations.h
@@ -0,0 +1,50 @@
+// SPDX-FileComment: This file is part of TNL - Template Numerical Library (https://tnl-project.org/)
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+namespace TNL::Algorithms::Segments {
+
+template< typename Segments, typename Index, typename Function >
+void
+forElements( const Segments&, Index begin, Index end, Function&& function );
+
+template< typename Segments, typename Function >
+void
+forAllElements( const Segments&, Function&& function );
+
+template< typename Segments, typename Array, typename IndexBegin, typename IndexEnd, typename Function >
+void
+forElements( const Segments&, const Array& segmentIndexes, IndexBegin begin, IndexEnd end, Function function );
+
+template< typename Segments, typename Array, typename Function >
+void
+forElements( const Segments&, const Array& segmentIndexes, Function function );
+
+template< typename Segments, typename IndexBegin, typename IndexEnd, typename Condition, typename Function >
+void
+forElementsIf( const Segments&, IndexBegin begin, IndexEnd end, Condition condition, Function function );
+
+template< typename Segments, typename Condition, typename Function >
+void
+forAllElementsIf( const Segments&, Condition condition, Function function );
+
+template< typename Segments, typename IndexBegin, typename IndexEnd, typename Function >
+void
+forSegments( const Segments&, IndexBegin begin, IndexEnd end, Function&& function );
+
+template< typename Segments, typename Function >
+void
+forAllSegments( const Segments&, Function&& function );
+
+template< typename Segments, typename IndexBegin, typename IndexEnd, typename Function >
+void
+sequentialForSegments( const Segments&, IndexBegin begin, IndexEnd end, Function&& function );
+
+template< typename Segments, typename Function >
+void
+sequentialForAllSegments( const Segments&, Function&& function );
+
+}  // namespace TNL::Algorithms::Segments
+
+#include "operations.hpp"
