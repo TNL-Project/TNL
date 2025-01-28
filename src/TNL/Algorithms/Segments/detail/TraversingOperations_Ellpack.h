7f8c5f4db3e5e34fbf74234428c3d016eab9a62a
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2025-01-28 17:12:58 +0100

    Renaming current segment operations to traversing operations.

--- src/TNL/Algorithms/Segments/detail/TraversingOperations_Ellpack.h
+++ src/TNL/Algorithms/Segments/detail/TraversingOperations_Ellpack.h
@@ -5,12 +5,12 @@
 
 #include <TNL/Algorithms/Segments/EllpackView.h>
 #include <TNL/Algorithms/Segments/Ellpack.h>
-#include "EllpackKernels.h"
+#include "TraversingKernels_Ellpack.h"
 
 namespace TNL::Algorithms::Segments::detail {
 
 template< typename Device, typename Index, ElementsOrganization Organization, int Alignment >
-struct SegmentsOperations< EllpackView< Device, Index, Organization, Alignment > >
+struct TraversingOperations< EllpackView< Device, Index, Organization, Alignment > >
 {
    using ViewType = EllpackView< Device, Index, Organization, Alignment >;
    // ViewType is the same as ConstViewType for Ellpack !!!!!
