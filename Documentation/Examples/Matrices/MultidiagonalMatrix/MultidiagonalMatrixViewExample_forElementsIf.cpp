17166a60ce2b532342e95dc25840010408f4b3dd
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2025-05-05 20:25:42 +0200

    Added forElementsIf and forElements with row indexes for multidiagonal matrices.

---
+++ Documentation/Examples/Matrices/MultidiagonalMatrix/MultidiagonalMatrixViewExample_forElementsIf.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <TNL/Matrices/MultidiagonalMatrix.h>
+#include <TNL/Devices/Host.h>
+#include <TNL/Devices/Cuda.h>
+
+template< typename Device >
+void
+forElementsExample()
+{
+   TNL::Matrices::MultidiagonalMatrix< double, Device > matrix( 5,              // number of matrix rows
+                                                                5,              // number of matrix columns
+                                                                { 0, 1, 2 } );  // matrix diagonals offsets
+
+   auto matrixView = matrix.getView();
+
+   auto condition = [] __cuda_callable__( int rowIdx )
+   {
+      return rowIdx % 2 == 0;
+   };
+   auto f = [] __cuda_callable__( int rowIdx, int localIdx, int columnIdx, double& value )
+   {
+      value = rowIdx + 1;
+   };
+
+   matrixView.forElementsIf( 0, matrix.getRows(), condition, f );  // or matrix.forElements
+   std::cout << matrix << std::endl;
+}
+
+int
+main( int argc, char* argv[] )
+{
+   std::cout << "Creating matrix on host: " << std::endl;
+   forElementsExample< TNL::Devices::Host >();
+
+#ifdef __CUDACC__
+   std::cout << "Creating matrix on CUDA device: " << std::endl;
+   forElementsExample< TNL::Devices::Cuda >();
+#endif
+}
---
+++ Documentation/Examples/Matrices/MultidiagonalMatrix/MultidiagonalMatrixViewExample_forElementsWithRowIndexes-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <TNL/Matrices/MultidiagonalMatrix.h>
+#include <TNL/Devices/Host.h>
+#include <TNL/Devices/Cuda.h>
+
+template< typename Device >
+void
+forElementsExample()
+{
+   TNL::Matrices::MultidiagonalMatrix< double, Device > matrix( 5,              // number of matrix rows
+                                                                5,              // number of matrix columns
+                                                                { 0, 1, 2 } );  // matrix diagonals offsets
+
+   auto matrixView = matrix.getView();
+
+   auto f = [] __cuda_callable__( int rowIdx, int localIdx, int columnIdx, double& value )
+   {
+      value = rowIdx + localIdx + 1;
+   };
+
+   TNL::Containers::Array< double, Device > rowIndexes{ 0, 2, 4, 0, 1, 3, 5, 9 };
+   // The following iterates only over the rows with indexes 0, 2, and 4, i.e. indexes
+   // at positions 0, 1, and 2 in the rowIndexes array. The rest is ignored.
+   matrixView.forElements( rowIndexes, 0, 2, f );  // or matrix.forElements
+   std::cout << matrix << std::endl;
+}
+
+int
+main( int argc, char* argv[] )
+{
+   std::cout << "Creating matrix on host: " << std::endl;
+   forElementsExample< TNL::Devices::Host >();
+
+#ifdef __CUDACC__
+   std::cout << "Creating matrix on CUDA device: " << std::endl;
+   forElementsExample< TNL::Devices::Cuda >();
+#endif
+}
