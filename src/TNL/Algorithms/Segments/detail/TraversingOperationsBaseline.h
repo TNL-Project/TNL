9ab1f639e507edaecc1ba4bd8f2e48a77fa2a0a2
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2025-05-29 18:48:38 +0200

    Moving segments traversing to TraversingOperationsBaseline.

--- src/TNL/Algorithms/Segments/detail/TraversingOperationsBaseline.h
+++ src/TNL/Algorithms/Segments/detail/TraversingOperationsBaseline.h
@@ -32,7 +32,73 @@ struct TraversingOperationsBaseline
          } );
 
       auto indexes = compressFast< VectorType >( conditions );
+      indexes += begin;
       forElements( segments, indexes, function, launchConfig );
+   }
+
+   template< typename IndexBegin, typename IndexEnd, typename Function >
+   static void
+   forSegments( const Segments& segments,
+                IndexBegin begin,
+                IndexEnd end,
+                Function&& function,
+                LaunchConfiguration launchConfig )
+   {
+      using IndexType = typename Segments::IndexType;
+      using DeviceType = typename Segments::DeviceType;
+      auto segments_view = segments.getConstView();
+      auto f = [ = ] __cuda_callable__( IndexType segmentIdx ) mutable
+      {
+         auto segment = segments_view.getSegmentView( segmentIdx );
+         function( segment );
+      };
+      Algorithms::parallelFor< DeviceType >( begin, end, f );  // TODO: Add launchConfig - it seems it does not work with
+                                                               // current implementation of parallelFor
+   }
+
+   template< typename Array, typename IndexBegin, typename IndexEnd, typename Function >
+   static void
+   forSegments( const Segments& segments,
+                const Array& segmentIndexes,
+                IndexBegin begin,
+                IndexEnd end,
+                Function&& function,
+                LaunchConfiguration launchConfig )
+   {
+      using IndexType = typename Segments::IndexType;
+      using DeviceType = typename Segments::DeviceType;
+      auto segments_view = segments.getConstView();
+      auto segmentIndexes_view = segmentIndexes.getConstView();
+      auto f = [ = ] __cuda_callable__( IndexType segmentIdx_idx ) mutable
+      {
+         TNL_ASSERT_LT( segmentIdx_idx, segmentIndexes_view.getSize(), "" );
+         TNL_ASSERT_LT( segmentIndexes_view[ segmentIdx_idx ], segments_view.getSegmentsCount(), "" );
+         auto segment = segments_view.getSegmentView( segmentIndexes_view[ segmentIdx_idx ] );
+         function( segment );
+      };
+      Algorithms::parallelFor< DeviceType >( begin, end, f );  // TODO: Add launchConfig - it seems it does not work with
+                                                               // current implementation of parallelFor
+   }
+
+   template< typename IndexBegin, typename IndexEnd, typename SegmentCondition, typename Function >
+   static void
+   forSegmentsIf( const Segments& segments,
+                  IndexBegin begin,
+                  IndexEnd end,
+                  SegmentCondition&& segmentCondition,
+                  Function&& function,
+                  LaunchConfiguration launchConfig )
+   {
+      using IndexType = typename Segments::IndexType;
+      using DeviceType = typename Segments::DeviceType;
+      auto segments_view = segments.getConstView();
+      auto f = [ = ] __cuda_callable__( IndexType segmentIdx ) mutable
+      {
+         if( segmentCondition( segmentIdx ) )
+            function( segments_view.getSegmentView( segmentIdx ) );
+      };
+      Algorithms::parallelFor< DeviceType >( begin, end, f );  // TODO: Add launchConfig - it seems it does not work with
+                                                               // current implementation of parallelFor
    }
 };
 
