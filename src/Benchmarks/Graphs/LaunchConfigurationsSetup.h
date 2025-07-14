5f601f60c53ababa54a73c870654685bdf3934c1
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2025-07-14 18:00:59 +0200

    Refactoring segments and graphs benchmarks - renaming type of launch configuration.

--- src/Benchmarks/Graphs/LaunchConfigurationsSetup.h
+++ src/Benchmarks/Graphs/LaunchConfigurationsSetup.h
@@ -8,17 +8,16 @@
 
 namespace TNL::Benchmarks::Graphs {
 
-using LaunchConfigurationType = TNL::Algorithms::Segments::LaunchConfiguration;
+using LaunchConfiguration = TNL::Algorithms::Segments::LaunchConfiguration;
 
 template< typename Segments >
 struct LaunchConfigurationsSetup
 {
    static auto
-   create() -> std::list< std::pair< LaunchConfigurationType, std::string > >
+   create() -> std::list< std::pair< LaunchConfiguration, std::string > >
    {
-      return std::list< std::pair< LaunchConfigurationType, std::string > >{
-         { LaunchConfigurationType( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::ThreadPerSegment, 1 ),
-           "ThreadPerSegment" }
+      return std::list< std::pair< LaunchConfiguration, std::string > >{
+         { LaunchConfiguration( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::ThreadPerSegment, 1 ), "ThreadPerSegment" }
       };
    }
 };
@@ -27,26 +26,25 @@ template< typename Device, typename Index, typename IndexAllocator >
 struct LaunchConfigurationsSetup< TNL::Algorithms::Segments::CSR< Device, Index, IndexAllocator > >
 {
    static auto
-   create() -> std::list< std::pair< LaunchConfigurationType, std::string > >
+   create() -> std::list< std::pair< LaunchConfiguration, std::string > >
    {
       if constexpr( std::is_same_v< Device, TNL::Devices::Host > || std::is_same_v< Device, TNL::Devices::Sequential > )
-         return std::list< std::pair< LaunchConfigurationType, std::string > >{
-            { LaunchConfigurationType( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::ThreadPerSegment, 1 ),
+         return std::list< std::pair< LaunchConfiguration, std::string > >{
+            { LaunchConfiguration( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::ThreadPerSegment, 1 ),
               "ThreadPerSegment" }
          };
       else
-         return std::list< std::pair< LaunchConfigurationType, std::string > >{
-            { LaunchConfigurationType( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::ThreadPerSegment, 1 ),
+         return std::list< std::pair< LaunchConfiguration, std::string > >{
+            { LaunchConfiguration( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::ThreadPerSegment, 1 ),
               "ThreadPerSegment" },
-            { LaunchConfigurationType( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::WarpPerSegment, 1 ),
-              "WarpPerSegment" },
-            { LaunchConfigurationType( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::BlockMergedSegments, 1 ),
+            { LaunchConfiguration( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::WarpPerSegment, 1 ), "WarpPerSegment" },
+            { LaunchConfiguration( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::BlockMergedSegments, 1 ),
               "BlockMergedSegments 1 thread per segment" },
-            { LaunchConfigurationType( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::BlockMergedSegments, 2 ),
+            { LaunchConfiguration( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::BlockMergedSegments, 2 ),
               "BlockMergedSegments 2 thread per segment" },
-            { LaunchConfigurationType( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::BlockMergedSegments, 4 ),
+            { LaunchConfiguration( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::BlockMergedSegments, 4 ),
               "BlockMergedSegments 4 thread per segment" },
-            { LaunchConfigurationType( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::BlockMergedSegments, 8 ),
+            { LaunchConfiguration( TNL::Algorithms::Segments::ThreadsToSegmentsMapping::BlockMergedSegments, 8 ),
               "BlockMergedSegments 8 thread per segment" }
          };
    }
