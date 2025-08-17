cb525d0c9c91f7e40bfa2e00747be65d0fcbc4c6
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2025-08-17 10:37:05 +0200

    Fixes of CuSolverWrapper.

--- src/TNL/Solvers/Linear/CuSolverWrapper.h
+++ src/TNL/Solvers/Linear/CuSolverWrapper.h
@@ -4,52 +4,30 @@
 #pragma once
 
 #ifdef __CUDACC__
-
    #include <cusolverSp.h>
    #include <cusolverDn.h>
+#endif
 
-   #include <TNL/Config/ConfigDescription.h>
-   #include <TNL/Matrices/SparseMatrix.h>
-   #include <TNL/Matrices/DenseMatrix.h>
-   #include <TNL/Solvers/Linear/LinearSolver.h>
+#include <TNL/Config/ConfigDescription.h>
+#include <TNL/Matrices/SparseMatrix.h>
+#include <TNL/Matrices/DenseMatrix.h>
+#include <TNL/Matrices/TypeTraits.h>
+#include <TNL/Solvers/Linear/LinearSolver.h>
 
 namespace TNL {
-
-template< typename Matrix >
-struct is_csr_matrix_type
-{
-   static constexpr bool value = false;
-};
-
-template< typename Real, typename Device, typename Index, typename ComputeReal, typename RealAllocator, typename IndexAllocator >
-struct is_csr_matrix_type< TNL::Matrices::SparseMatrix< Real,
-                                                        Device,
-                                                        Index,
-                                                        Matrices::GeneralMatrix,
-                                                        Algorithms::Segments::CSR,
-                                                        ComputeReal,
-                                                        RealAllocator,
-                                                        IndexAllocator > >
-{
-   static constexpr bool value = true;
-};
-
-template< typename Matrix >
-constexpr bool is_csr_matrix_type_v = is_csr_matrix_type< Matrix >::value;
 
 namespace Solvers::Linear {
 
 template< typename Matrix >
 class CuSolverWrapper : public LinearSolver< Matrix >
 {
-   static_assert( is_csr_matrix_type_v< Matrix > || Matrices::is_dense_matrix_type_v< Matrix >,
+   static_assert( Matrices::is_sparse_csr_matrix_v< Matrix > || Matrices::is_dense_matrix_v< Matrix >,
                   "The CuSolverWrapper solver is available only for CSR and dense matrices." );
-   static_assert( std::is_same< typename Matrix::DeviceType, Devices::Cuda >::value,
-                  "CuSolverWrapper is available only on CUDA" );
-   static_assert( std::is_same< typename Matrix::RealType, float >::value
-                     || std::is_same< typename Matrix::RealType, double >::value,
+   static_assert( std::is_same_v< typename Matrix::DeviceType, Devices::Cuda >, "CuSolverWrapper is available only on CUDA" );
+   static_assert( std::is_same_v< typename Matrix::RealType, float > || std::is_same_v< typename Matrix::RealType, double >,
                   "unsupported RealType" );
-   static_assert( std::is_same< typename Matrix::IndexType, int >::value, "unsupported IndexType" );
+   static_assert( Matrices::is_dense_matrix_v< Matrix > || std::is_same_v< typename Matrix::IndexType, int >,
+                  "unsupported IndexType" );
 
    using Base = LinearSolver< Matrix >;
 
@@ -63,7 +41,7 @@ public:
    bool
    solve( ConstVectorViewType b, VectorViewType x ) override
    {
-      if constexpr( is_csr_matrix_type_v< Matrix > )
+      if constexpr( Matrices::is_sparse_csr_matrix_v< Matrix > )
          return solveSparse( b, x );
       else  // is_dense_matrix_v< Matrix > must be true
          return solveDense( b, x );
@@ -72,6 +50,10 @@ public:
    bool
    solveDense( ConstVectorViewType b, VectorViewType x )
    {
+#ifdef __CUDACC__
+      static_assert( Matrices::is_dense_matrix_v< Matrix >,
+                     "CuSolverWrapper::solveDense can be called only for dense matrices." );
+
       TNL_ASSERT_EQ( this->matrix->getRows(), this->matrix->getColumns(), "matrix must be square" );
       TNL_ASSERT_EQ( this->matrix->getColumns(), x.getSize(), "wrong size of the solution vector" );
       TNL_ASSERT_EQ( this->matrix->getColumns(), b.getSize(), "wrong size of the right hand side" );
@@ -82,18 +64,18 @@ public:
       cusolverStatus_t status = cusolverDnCreate( &handle );
       if( status != CUSOLVER_STATUS_SUCCESS ) {
          std::cerr << "cusolverDnCreate failed: " << status << std::endl;
+         cusolverDnDestroy( handle );
          return false;
       }
 
       x = b;
-      int* d_info;
-      cudaMalloc( &d_info, sizeof( int ) );
+      Containers::Vector< int, Devices::Cuda > d_info( 1 );
 
       int lwork = 0;
-      if constexpr( std::is_same< RealType, float >::value )
+      if constexpr( std::is_same_v< RealType, float > )
          status =
             cusolverDnSgetrf_bufferSize( handle, n, n, const_cast< float* >( this->matrix->getValues().getData() ), n, &lwork );
-      else if constexpr( std::is_same< RealType, double >::value )
+      else if constexpr( std::is_same_v< RealType, double > )
          status = cusolverDnDgetrf_bufferSize(
             handle, n, n, const_cast< double* >( this->matrix->getValues().getData() ), n, &lwork );
 
@@ -101,7 +83,7 @@ public:
       Containers::Vector< int, Devices::Cuda > d_pivots( n );
 
       // LU factorization
-      if constexpr( std::is_same< RealType, float >::value )
+      if constexpr( std::is_same_v< RealType, float > )
          status = cusolverDnSgetrf( handle,
                                     n,
                                     n,
@@ -109,8 +91,8 @@ public:
                                     n,
                                     d_work.getData(),
                                     d_pivots.getData(),
-                                    d_info );
-      else if constexpr( std::is_same< RealType, double >::value )
+                                    d_info.getData() );
+      else if constexpr( std::is_same_v< RealType, double > )
          status = cusolverDnDgetrf( handle,
                                     n,
                                     n,
@@ -118,10 +100,10 @@ public:
                                     n,
                                     d_work.getData(),
                                     d_pivots.getData(),
-                                    d_info );
+                                    d_info.getData() );
 
       // Solve Ax = b
-      if constexpr( std::is_same< RealType, float >::value )
+      if constexpr( std::is_same_v< RealType, float > )
          status = cusolverDnSgetrs( handle,
                                     CUBLAS_OP_N,
                                     n,
@@ -131,8 +113,8 @@ public:
                                     d_pivots.getData(),
                                     x.getData(),
                                     n,
-                                    d_info );
-      else if constexpr( std::is_same< RealType, double >::value )
+                                    d_info.getData() );
+      else if constexpr( std::is_same_v< RealType, double > )
          status = cusolverDnDgetrs( handle,
                                     CUBLAS_OP_N,
                                     n,
@@ -142,15 +124,21 @@ public:
                                     d_pivots.getData(),
                                     x.getData(),
                                     n,
-                                    d_info );
-
-      cudaFree( d_info );
+                                    d_info.getData() );
+      cusolverDnDestroy( handle );
       return true;
+#else
+      throw std::runtime_error( "CuSolverWrapper was not built with CUDA support." );
+#endif
    }
 
    bool
    solveSparse( ConstVectorViewType b, VectorViewType x )
    {
+#ifdef __CUDACC__
+      static_assert( Matrices::is_sparse_csr_matrix_v< Matrix >,
+                     "CuSolverWrapper::solveSparse can be called only for sparse CSR matrices." );
+
       TNL_ASSERT_EQ( this->matrix->getRows(), this->matrix->getColumns(), "matrix must be square" );
       TNL_ASSERT_EQ( this->matrix->getColumns(), x.getSize(), "wrong size of the solution vector" );
       TNL_ASSERT_EQ( this->matrix->getColumns(), b.getSize(), "wrong size of the right hand side" );
@@ -168,12 +156,16 @@ public:
       status = cusolverSpCreate( &handle );
       if( status != CUSOLVER_STATUS_SUCCESS ) {
          std::cerr << "cusolverSpCreate failed: " << status << std::endl;
+         cusparseDestroyMatDescr( mat_descr );
+         cusolverSpDestroy( handle );
          return false;
       }
 
       cusparse_status = cusparseCreateMatDescr( &mat_descr );
       if( cusparse_status != CUSPARSE_STATUS_SUCCESS ) {
          std::cerr << "cusparseCreateMatDescr failed: " << cusparse_status << std::endl;
+         cusparseDestroyMatDescr( mat_descr );
+         cusolverSpDestroy( handle );
          return false;
       }
 
@@ -184,7 +176,7 @@ public:
       const int reorder = 0;
       int singularity = 0;
 
-      if( std::is_same< typename Matrix::RealType, float >::value ) {
+      if( std::is_same_v< typename Matrix::RealType, float > ) {
          status = cusolverSpScsrlsvqr( handle,
                                        size,
                                        this->matrix->getValues().getSize(),
@@ -200,11 +192,13 @@ public:
 
          if( status != CUSOLVER_STATUS_SUCCESS ) {
             std::cerr << "cusolverSpScsrlsvqr failed: " << status << std::endl;
+            cusparseDestroyMatDescr( mat_descr );
+            cusolverSpDestroy( handle );
             return false;
          }
       }
 
-      if( std::is_same< typename Matrix::RealType, double >::value ) {
+      if( std::is_same_v< typename Matrix::RealType, double > ) {
          status = cusolverSpDcsrlsvqr( handle,
                                        size,
                                        this->matrix->getValues().getSize(),
@@ -220,18 +214,20 @@ public:
 
          if( status != CUSOLVER_STATUS_SUCCESS ) {
             std::cerr << "cusolverSpDcsrlsvqr failed: " << status << std::endl;
+            cusparseDestroyMatDescr( mat_descr );
+            cusolverSpDestroy( handle );
             return false;
          }
       }
 
       cusparseDestroyMatDescr( mat_descr );
       cusolverSpDestroy( handle );
-
       return true;
+#else
+      throw std::runtime_error( "CuSolverWrapper was not built with CUDA support." );
+#endif
    }
 };
 
 }  // namespace Solvers::Linear
 }  // namespace TNL
-
-#endif
