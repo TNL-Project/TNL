8e5260eb2382bb3a882ca29a3f719d60cf8b6024
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2025-10-06 10:05:03 +0200

    Added test for getView in segments.

--- src/UnitTests/Algorithms/Segments/SegmentsTest_AdaptiveCSR.h
+++ src/UnitTests/Algorithms/Segments/SegmentsTest_AdaptiveCSR.h
@@ -34,6 +34,11 @@ TYPED_TEST( AdaptiveCSRSegmentsTest, isSegments )
    test_isSegments< typename TestFixture::SegmentsType >();
 }
 
+TYPED_TEST( AdaptiveCSRSegmentsTest, getView )
+{
+   test_getView< typename TestFixture::SegmentsType >();
+}
+
 TYPED_TEST( AdaptiveCSRSegmentsTest, setSegmentsSizes_EqualSizes )
 {
    test_setSegmentsSizes_EqualSizes< typename TestFixture::SegmentsType >();
