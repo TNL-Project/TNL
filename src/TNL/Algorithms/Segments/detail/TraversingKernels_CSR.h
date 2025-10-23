f4dfb3d577673a2e11c52c37a67743e6b39c86b1
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2025-10-23 18:13:37 +0200

    Added dynamic-grouping traversing kernels to CSR segments.

--- src/TNL/Algorithms/Segments/detail/TraversingKernels_CSR.h
+++ src/TNL/Algorithms/Segments/detail/TraversingKernels_CSR.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include <TNL/Algorithms/AtomicOperations.h>
+
 //#define USE_CUB
 #ifdef __CUDACC__
    #include <cub/cub.cuh>
@@ -11,9 +13,140 @@
 namespace TNL::Algorithms::Segments::detail {
 
 template< typename OffsetsView, typename Index, typename Function, int BlockSize = 256 >
+__device__
+void
+dynamicGroupingTraversingKernel_CSR( const OffsetsView offsets,
+                                     bool traverse_segment,
+                                     const Index segmentIdx,
+                                     Function function )
+{
+#if defined( __CUDACC__ ) || defined( __HIP__ )
+   constexpr Index warpSize = Backend::getWarpSize();
+   constexpr Index warpsPerBlock = BlockSize / warpSize;
+   __shared__ Index warps_scheduler[ BlockSize ];
+
+   // Processing segments larger than BlockSize
+   __shared__ Index scheduled_segment[ 1 ];
+   constexpr Index none_scheduled = -1;
+   Index segment_size = -1;
+   if( traverse_segment ) {
+      segment_size = offsets[ segmentIdx + 1 ] - offsets[ segmentIdx ];
+   }
+
+   if( threadIdx.x == 0 )
+      scheduled_segment[ 0 ] = none_scheduled;
+   __syncthreads();
+   while( true ) {
+      if( traverse_segment && segment_size > BlockSize ) {
+         AtomicOperations< Devices::GPU >::CAS( scheduled_segment[ 0 ], scheduled_segment[ 0 ], segmentIdx );
+      }
+      __syncthreads();
+      if( scheduled_segment[ 0 ] == none_scheduled )
+         break;
+
+      Index globalIdx = offsets[ scheduled_segment[ 0 ] ];
+      const Index endIdx = offsets[ scheduled_segment[ 0 ] + 1 ];
+
+      if constexpr( argumentCount< Function >() == 3 ) {
+         Index localIdx = threadIdx.x;
+         while( globalIdx < endIdx ) {
+            function( scheduled_segment[ 0 ], localIdx, globalIdx );
+            localIdx += BlockSize;
+            globalIdx += BlockSize;
+         }
+      }
+      else
+         while( globalIdx < endIdx ) {
+            function( scheduled_segment[ 0 ], globalIdx );
+            globalIdx += BlockSize;
+         }
+      if( segmentIdx == scheduled_segment[ 0 ] ) {
+         traverse_segment = false;
+         scheduled_segment[ 0 ] = none_scheduled;
+      }
+      __syncthreads();
+   }
+
+   // Processing segments smaller than BlockSize and larger the warp size
+   __shared__ int active_warps[ 1 ];
+   if( threadIdx.x == 0 )
+      active_warps[ 0 ] = 0;
+   __syncthreads();
+
+   // Each thread owning segment with size larger than warpSize registers for scheduling
+   if( traverse_segment && segment_size > warpSize )
+      warps_scheduler[ AtomicOperations< Devices::GPU >::add( active_warps[ 0 ], 1 ) ] = segmentIdx;
+   __syncthreads();
+
+   // Now traverse scheduled segments in warps
+   Index warp_idx = threadIdx.x / warpSize;
+   while( warp_idx < active_warps[ 0 ] ) {
+      Index scheduled_segment = warps_scheduler[ warp_idx ];
+      Index globalIdx = offsets[ scheduled_segment ] + ( threadIdx.x & ( warpSize - 1 ) );  // & is cheaper than %
+      const Index endIdx = offsets[ scheduled_segment + 1 ];
+      if constexpr( argumentCount< Function >() == 3 ) {
+         Index localIdx = threadIdx.x & ( warpSize - 1 );  // & is cheaper than %
+         for( ; globalIdx < endIdx; globalIdx += warpSize ) {
+            function( scheduled_segment, localIdx, globalIdx );
+            localIdx += warpSize;
+         }
+      }
+      else {
+         for( ; globalIdx < endIdx; globalIdx += warpSize ) {
+            function( scheduled_segment, globalIdx );
+         }
+      }
+      if( segmentIdx == scheduled_segment ) {
+         traverse_segment = false;
+      }
+      warp_idx += warpsPerBlock;
+   }
+
+   // Processing segments smaller than or equal to warp size
+   if( traverse_segment ) {
+      Index globalIdx = offsets[ segmentIdx ];
+      const Index endIdx = offsets[ segmentIdx + 1 ];
+      if constexpr( argumentCount< Function >() == 3 ) {
+         Index localIdx = 0;
+         for( ; globalIdx < endIdx; globalIdx++ ) {
+            function( segmentIdx, localIdx, globalIdx );
+            localIdx++;
+         }
+      }
+      else {
+         for( ; globalIdx < endIdx; globalIdx++ ) {
+            function( segmentIdx, globalIdx );
+         }
+      }
+   }
+#endif
+}
+
+template< typename OffsetsView, typename Index, typename Function, int BlockSize = 256 >
 __global__
 void
-forElementsBlockMergeKernel_CSR( Index gridIdx, OffsetsView offsets, Index begin, Index end, Function function )
+forElementsDynamicGroupingKernel_CSR( const Index gridIdx,
+                                      const OffsetsView offsets,
+                                      const Index begin,
+                                      const Index end,
+                                      Function function )
+{
+#if defined( __CUDACC__ ) || defined( __HIP__ )
+   const Index segmentIdx = begin + Backend::getGlobalThreadIdx_x( gridIdx );
+   bool traverse_segment = ( segmentIdx < end );
+
+   dynamicGroupingTraversingKernel_CSR( offsets, traverse_segment, segmentIdx, function );
+#endif
+}
+
+template< typename OffsetsView, typename Index, typename Function, int BlockSize = 256 >
+__global__
+void
+forElementsBlockMergeKernel_CSR( const Index gridIdx,
+                                 const OffsetsView offsets,
+                                 const Index begin,
+                                 const Index end,
+                                 Function function )
 {
 #if defined( __CUDACC__ ) || defined( __HIP__ )
 
@@ -51,27 +184,30 @@ forElementsBlockMergeKernel_CSR( Index gridIdx, OffsetsView offsets, Index begin
 template< typename OffsetsView, typename Index, typename Function >
 __global__
 void
-forElementsKernel_CSR( Index gridIdx, OffsetsView offsets, Index begin, Index end, Function function )
+forElementsKernel_CSR( const Index gridIdx,
+                       const Index threadsPerSegment,
+                       OffsetsView offsets,
+                       const Index begin,
+                       const Index end,
+                       Function function )
 {
 #if defined( __CUDACC__ ) || defined( __HIP__ )
-
-   // We map one warp to each segment
-   const Index segmentIdx = begin + Backend::getGlobalThreadIdx_x( gridIdx ) / Backend::getWarpSize();
+   const Index segmentIdx = begin + Backend::getGlobalThreadIdx_x( gridIdx ) / threadsPerSegment;
    if( segmentIdx >= end )
       return;
 
-   const Index laneIdx = threadIdx.x & ( Backend::getWarpSize() - 1 );  // & is cheaper than %
+   const Index laneIdx = threadIdx.x & ( threadsPerSegment - 1 );  // & is cheaper than %
    TNL_ASSERT_LT( segmentIdx + 1, offsets.getSize(), "" );
    Index endIdx = offsets[ segmentIdx + 1 ];
 
    Index localIdx = laneIdx;
-   for( Index globalIdx = offsets[ segmentIdx ] + laneIdx; globalIdx < endIdx; globalIdx += Backend::getWarpSize() ) {
+   for( Index globalIdx = offsets[ segmentIdx ] + laneIdx; globalIdx < endIdx; globalIdx += threadsPerSegment ) {
       TNL_ASSERT_LT( globalIdx, endIdx, "" );
       if constexpr( argumentCount< Function >() == 3 )
          function( segmentIdx, localIdx, globalIdx );
       else
          function( segmentIdx, globalIdx );
-      localIdx += Backend::getWarpSize();
+      localIdx += threadsPerSegment;
    }
 #endif
 }
@@ -79,17 +215,17 @@ forElementsKernel_CSR( Index gridIdx, OffsetsView offsets, Index begin, Index en
 template< typename OffsetsView, typename ArrayView, typename Index, typename Function >
 __global__
 void
-forElementsWithSegmentIndexesKernel_CSR( Index gridIdx,
-                                         OffsetsView offsets,
-                                         ArrayView segmentIndexes,
-                                         Index begin,
-                                         Index end,
+forElementsWithSegmentIndexesKernel_CSR( const Index gridIdx,
+                                         const Index threadsPerSegment,
+                                         const OffsetsView offsets,
+                                         const ArrayView segmentIndexes,
+                                         const Index begin,
+                                         const Index end,
                                          Function function )
 {
 #if defined( __CUDACC__ ) || defined( __HIP__ )
 
-   // We map one warp to each segment
-   const Index idx = begin + Backend::getGlobalThreadIdx_x( gridIdx ) / Backend::getWarpSize();
+   const Index idx = begin + Backend::getGlobalThreadIdx_x( gridIdx ) / threadsPerSegment;
    if( idx >= end )
       return;
    TNL_ASSERT_GE( idx, 0, "" );
@@ -98,28 +234,57 @@ forElementsWithSegmentIndexesKernel_CSR( Index gridIdx,
    TNL_ASSERT_GE( segmentIdx, 0, "Wrong index segment index - smaller that 0." );
    TNL_ASSERT_LT( segmentIdx, offsets.getSize() - 1, "Wrong index segment index - larger that the number of indexes." );
 
-   const Index laneIdx = threadIdx.x & ( Backend::getWarpSize() - 1 );  // & is cheaper than %
+   const Index laneIdx = threadIdx.x & ( threadsPerSegment - 1 );  // & is cheaper than %
    TNL_ASSERT_LT( segmentIdx + 1, offsets.getSize(), "" );
    Index endIdx = offsets[ segmentIdx + 1 ];
 
    Index localIdx = laneIdx;
-   for( Index globalIdx = offsets[ segmentIdx ] + laneIdx; globalIdx < endIdx; globalIdx += Backend::getWarpSize() ) {
+   for( Index globalIdx = offsets[ segmentIdx ] + laneIdx; globalIdx < endIdx; globalIdx += threadsPerSegment ) {
       TNL_ASSERT_LT( globalIdx, endIdx, "" );
       if constexpr( argumentCount< Function >() == 3 )
          function( segmentIdx, localIdx, globalIdx );
       else
          function( segmentIdx, globalIdx );
-      localIdx += Backend::getWarpSize();
+      localIdx += threadsPerSegment;
    }
+#endif
+}
+
+template< typename OffsetsView, typename ArrayView, typename Index, typename Function, int BlockSize = 256 >
+__global__
+void
+forElementsWithSegmentIndexesDynamicGroupingKernel_CSR( const Index gridIdx,
+                                                        const OffsetsView offsets,
+                                                        const ArrayView segmentIndexes,
+                                                        const Index begin,
+                                                        const Index end,
+                                                        Function function )
+{
+#if defined( __CUDACC__ ) || defined( __HIP__ )
+
+   const Index segmentIdx_ptr = begin + Backend::getGlobalThreadIdx_x( gridIdx );
+
+   Index segmentIdx;
+   bool traverse_segment( false );
+   if( segmentIdx_ptr < end ) {
+      TNL_ASSERT_GE( segmentIdx_ptr, 0, "" );
+      TNL_ASSERT_LT( segmentIdx_ptr, segmentIndexes.getSize(), "" );
+      segmentIdx = segmentIndexes[ segmentIdx_ptr ];
+      TNL_ASSERT_GE( segmentIdx, 0, "Wrong index of segment index - smaller that 0." );
+      TNL_ASSERT_LT( segmentIdx, offsets.getSize() - 1, "Wrong index of segment index - larger that the number of indexes." );
+      traverse_segment = true;
+   }
+
+   dynamicGroupingTraversingKernel_CSR( offsets, traverse_segment, segmentIdx, function );
 #endif
 }
 
 template< typename OffsetsView, typename ArrayView, typename Index, typename Function, int SegmentsPerBlock, int BlockSize = 256 >
 __global__
 void
-forElementsWithSegmentIndexesBlockMergeKernel_CSR( Index gridIdx,
-                                                   OffsetsView offsets,
-                                                   ArrayView segmentIndexes,
+forElementsWithSegmentIndexesBlockMergeKernel_CSR( const Index gridIdx,
+                                                   const OffsetsView offsets,
+                                                   const ArrayView segmentIndexes,
                                                    const Index begin,
                                                    const Index end,
                                                    Function function )
@@ -211,30 +376,59 @@ forElementsWithSegmentIndexesBlockMergeKernel_CSR( Index gridIdx,
 template< typename OffsetsView, typename Index, typename Condition, typename Function >
 __global__
 void
-forElementsIfKernel_CSR( Index gridIdx, OffsetsView offsets, Index begin, Index end, Condition condition, Function function )
+forElementsIfKernel_CSR( const Index gridIdx,
+                         const Index threadsPerSegment,
+                         const OffsetsView offsets,
+                         const Index begin,
+                         const Index end,
+                         Condition condition,
+                         Function function )
 {
 #if defined( __CUDACC__ ) || defined( __HIP__ )
 
-   // We map one warp to each segment
-   const Index segmentIdx = begin + Backend::getGlobalThreadIdx_x( gridIdx ) / Backend::getWarpSize();
+   const Index segmentIdx = begin + Backend::getGlobalThreadIdx_x( gridIdx ) / threadsPerSegment;
    if( segmentIdx >= end || ! condition( segmentIdx ) )
       return;
 
-   const Index laneIdx = threadIdx.x & ( Backend::getWarpSize() - 1 );  // & is cheaper than %
+   const Index laneIdx = threadIdx.x & ( threadsPerSegment - 1 );  // & is cheaper than %
    TNL_ASSERT_LT( segmentIdx + 1, offsets.getSize(), "" );
    Index endIdx = offsets[ segmentIdx + 1 ];
 
    Index localIdx = laneIdx;
-   for( Index globalIdx = offsets[ segmentIdx ] + laneIdx; globalIdx < endIdx; globalIdx += Backend::getWarpSize() ) {
+   for( Index globalIdx = offsets[ segmentIdx ] + laneIdx; globalIdx < endIdx; globalIdx += threadsPerSegment ) {
       TNL_ASSERT_LT( globalIdx, endIdx, "" );
       if constexpr( argumentCount< Function >() == 3 )
          function( segmentIdx, localIdx, globalIdx );
       else
          function( segmentIdx, globalIdx );
-      localIdx += Backend::getWarpSize();
+      localIdx += threadsPerSegment;
    }
 #endif
 }
+
+template< typename OffsetsView, typename Index, typename Condition, typename Function, int BlockSize = 256 >
+__global__
+void
+forElementsIfDynamicGroupingKernel_CSR( const Index gridIdx,
+                                        const OffsetsView offsets,
+                                        const Index begin,
+                                        const Index end,
+                                        Condition condition,
+                                        Function function )
+{
+#if defined( __CUDACC__ ) || defined( __HIP__ )
+
+   const Index segmentIdx = begin + Backend::getGlobalThreadIdx_x( gridIdx );
+
+   bool traverse_segment = false;
+   if( segmentIdx < end ) {
+      traverse_segment = condition( segmentIdx );
+   }
+
+   dynamicGroupingTraversingKernel_CSR( offsets, traverse_segment, segmentIdx, function );
+#endif
+}
+
 template< typename OffsetsView, typename Index, typename Condition, typename Function, int SegmentsPerBlock, int BlockSize = 256 >
 __global__
 void
