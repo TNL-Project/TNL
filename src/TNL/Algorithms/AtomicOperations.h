8f03f491010d7bb106740cb1ce42eb9fc69e9376
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2025-10-24 15:43:10 +0200

    Added atomic CAS.

--- src/TNL/Algorithms/AtomicOperations.h
+++ src/TNL/Algorithms/AtomicOperations.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include <mutex>
+
 #include <TNL/Atomic.h>
 #include <TNL/Assert.h>
 
@@ -25,13 +27,46 @@ struct AtomicOperations< Devices::Host >
    {
       Value old;
 #ifdef HAVE_OPENMP
-      #pragma omp atomic capture
+   #pragma omp atomic capture
 #endif
       {
          old = v;
          v += a;
       }
       return old;
+   }
+
+   // this is __cuda_callable__ only to silence nvcc warnings (all methods inside class
+   // template specializations must have the same execution space specifier, otherwise
+   // nvcc complains)
+   TNL_NVCC_HD_WARNING_DISABLE
+   template< typename Value >
+   __cuda_callable__
+   static Value
+   CAS( Value& address, Value compare, Value val )
+   {
+#ifdef HAVE_OPENMP
+      // initialize the OpenMP lock exactly once without using a critical section
+      static omp_lock_t cas_lock;
+      static std::once_flag cas_lock_once;
+      std::call_once( cas_lock_once,
+                      []()
+                      {
+                         omp_init_lock( &cas_lock );
+                      } );
+
+      omp_set_lock( &cas_lock );
+      Value old = address;
+      if( old == compare )
+         address = val;
+      omp_unset_lock( &cas_lock );
+      return old;
+#else
+      const Value old = address;
+      if( old == compare )
+         address = val;
+      return old;
+#endif
    }
 };
 
@@ -49,6 +84,21 @@ struct AtomicOperations< Devices::Sequential >
    {
       const Value old = v;
       v += a;
+      return old;
+   }
+
+   // this is __cuda_callable__ only to silence nvcc warnings (all methods inside class
+   // template specializations must have the same execution space specifier, otherwise
+   // nvcc complains)
+   TNL_NVCC_HD_WARNING_DISABLE
+   template< typename Value >
+   __cuda_callable__
+   static Value
+   CAS( Value& address, Value compare, Value val )
+   {
+      const Value old = address;
+      if( old == compare )
+         address = val;
       return old;
    }
 };
@@ -78,6 +128,25 @@ struct AtomicOperations< Devices::Cuda >
 #endif
       return 0;
    }
-};
 
+   template< typename Value >
+   __cuda_callable__
+   static Value
+   CAS( Value& address, Value compare, Value val )
+   {
+#if defined( __CUDA_ARCH__ ) || defined( __HIP_DEVICE_COMPILE__ )
+      if constexpr( sizeof( Value ) == 4 )
+         return (Value) atomicCAS( (int*) &address, (int) compare, (int) val );
+      else if constexpr( sizeof( Value ) == 8 )
+         return (Value) atomicCAS(
+            (unsigned long long int*) &address, (unsigned long long int) compare, (unsigned long long int) val );
+      else {
+         TNL_ASSERT_TRUE( false, "Atomic CAS is supported only for 4 and 8 byte values on CUDA." );
+         return Value();
+      }
+#else
+      return 0;
+#endif
+   }
+};
 }  // namespace TNL::Algorithms
