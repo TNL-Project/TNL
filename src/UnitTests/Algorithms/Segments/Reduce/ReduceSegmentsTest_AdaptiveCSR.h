c5ece637eddceff9eda8f6e9febe2da17d3cbfd5
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2025-11-26 13:38:09 +0100

    Fixing dynamic grouping kernel for CSR segment reduction.

--- src/UnitTests/Algorithms/Segments/Reduce/ReduceSegmentsTest_AdaptiveCSR.h
+++ src/UnitTests/Algorithms/Segments/Reduce/ReduceSegmentsTest_AdaptiveCSR.h
@@ -34,6 +34,11 @@ TYPED_TEST( AdaptiveCSRReduceSegmentsTest, reduceSegments_MaximumInSegments )
    test_reduceSegments_MaximumInSegments< typename TestFixture::SegmentsType >();
 }
 
+TYPED_TEST( AdaptiveCSRReduceSegmentsTest, reduceSegments_MaximumInTriangularSegments )
+{
+   test_reduceSegments_MaximumInTriangularSegments< typename TestFixture::SegmentsType >();
+}
+
 TYPED_TEST( AdaptiveCSRReduceSegmentsTest, reduceSegments_MaximumInSegments_short_fetch )
 {
    test_reduceSegments_MaximumInSegments_short_fetch< typename TestFixture::SegmentsType >();
