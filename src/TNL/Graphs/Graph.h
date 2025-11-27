e0b88d030075eda98e7e822297094b5f13326459
Tomas Oberhuber <tomas.oberhuber@fjfi.cvut.cz>
2025-11-27 13:51:15 +0100

    Refactoring code for graphs.

--- src/TNL/Graphs/Graph.h
+++ src/TNL/Graphs/Graph.h
@@ -11,204 +11,191 @@
 
 namespace TNL::Graphs {
 
+//! Enumeration of supported graph types - directed or undirected.
 enum class GraphTypes : std::uint8_t
 {
    Directed,   //!< Directed graphs.
    Undirected  //!< Undirected graphs.
 };
 
+/**
+ * \brief \e Graph class represents a mathematical graph using an adjacency matrix.
+ *
+ * \tparam Matrix is type of matrix used to store the adjacency matrix of the graph.
+ * \tparam GraphType is type of the graph - directed or undirected.
+ */
 template< typename Matrix, GraphTypes GraphType = GraphTypes::Directed >
 struct Graph
 {
    static_assert( Matrices::is_matrix_v< Matrix > );
 
+   //! \brief Type of the adjacency matrix.
    using MatrixType = Matrix;
+
+   //! \brief Type for indexing of the graph nodes.
    using IndexType = typename Matrix::IndexType;
+
+   //! \brief Type of device where the graph will be operating.
    using DeviceType = typename Matrix::DeviceType;
+
+   //! \brief Type for weights of the graph edges.
    using ValueType = typename Matrix::RealType;
 
+   //! \brief Checks if the graph is directed.
    static constexpr bool
-   isDirected()
-   {
-      return GraphType == GraphTypes::Directed;
-   }  // TODO: Use getGraphType() instead
+   isDirected();
 
+   //! \brief Checks if the graph is undirected.
    static constexpr bool
-   isUndirected()
-   {
-      return GraphType == GraphTypes::Undirected;
-   }  // TODO: Use getGraphType() instead
+   isUndirected();
 
+   //! \brief Returns the type of the graph.
    static constexpr GraphTypes
-   getGraphType()
-   {
-      return GraphType;
-   }
+   getGraphType();
 
+   //! \brief Default constructor.
    Graph() = default;
 
-   Graph( const MatrixType& matrix )
-   {
-      this->adjacencyMatrix = matrix;
-   }
+   //! \brief Constructor with adjacency matrix.
+   Graph( const MatrixType& matrix );
 
-   Graph( MatrixType&& matrix )
-   : MatrixType( std::move( matrix ) )
-   {}
+   //! \brief Constructor with adjacency matrix.
+   Graph( MatrixType&& matrix );
 
+   //! \brief Copy constructor.
    Graph( const Graph& ) = default;
 
+   //! \brief Move constructor.
    Graph( Graph&& ) = default;
 
+   //! \brief Templated copy constructor from another graph type.
    template< typename OtherGraph >
-   Graph( const OtherGraph& other )
-   {
-      this->adjacencyMatrix = other.getAdjacencyMatrix();
-   }
+   Graph( const OtherGraph& other );
 
+   //! \brief Templated move constructor from another graph type.
    template< typename OtherGraph >
-   Graph( const OtherGraph&& other )
-   : MatrixType( std::forward< typename OtherGraph::MatrixType >( other.getAdjacencyMatrix() ) )
-   {}
+   Graph( const OtherGraph&& other );
 
+   /**
+    * \brief Constructor with number of nodes and edges given as initializer list.
+    *
+    * \param nodesCount is the number of nodes in the graph.
+    * \param data is the initializer list of tuples (source node, target node, edge weight).
+    * \param encoding is the encoding for symmetric matrices (used only for undirected graphs).
+    *
+    * If the graph is undirected, the adjacency matrix can be symmetric. In this case, the
+    * parameter \a encoding specifies whether the lower or upper part of the matrix is provided.
+    * If the graph is undirected and the adjacency matrix type is not symmetric, the constructor
+    * will create a symmetric adjacency matrix by adding both (source, target) and (target, source)
+    * entries for each edge.
+    */
    Graph( IndexType nodesCount,
           const std::initializer_list< std::tuple< IndexType, IndexType, ValueType > >& data,
-          Matrices::SymmetricMatrixEncoding encoding = Matrices::SymmetricMatrixEncoding::LowerPart )
-   {
-      if( isUndirected() && ! MatrixType::isSymmetric() ) {
-         std::map< std::pair< IndexType, IndexType >, ValueType > symmetric_map;
-         for( const auto& [ source, target, weight ] : data ) {
-            symmetric_map[ { source, target } ] = weight;
-            symmetric_map[ { target, source } ] = weight;
-         }
-         this->adjacencyMatrix.setDimensions( nodesCount, nodesCount );
-         this->adjacencyMatrix.setElements( symmetric_map );
-      }
-      else {
-         this->adjacencyMatrix.setDimensions( nodesCount, nodesCount );
-         this->adjacencyMatrix.setElements( data, encoding );
-      }
-   }
+          Matrices::SymmetricMatrixEncoding encoding = Matrices::SymmetricMatrixEncoding::LowerPart );
 
+   /**
+    * \brief Constructor with number of nodes and edges given as a map.
+    *
+    * \param nodesCount is the number of nodes in the graph.
+    * \param map is the map with keys as (source node, target node) pairs and values as edge weights.
+    * \param encoding is the encoding for symmetric matrices (used only for undirected graphs).
+    *
+    * If the graph is undirected, the adjacency matrix can be symmetric. In this case, the
+    * parameter \a encoding specifies whether the lower or upper part of the matrix is provided.
+    * If the graph is undirected and the adjacency matrix type is not symmetric, the constructor
+    * will create a symmetric adjacency matrix by adding both (source, target) and (target, source)
+    * entries for each edge.
+    */
    template< typename MapIndex, typename MapValue >
    Graph( IndexType nodesCount,
           const std::map< std::pair< MapIndex, MapIndex >, MapValue >& map,
-          Matrices::SymmetricMatrixEncoding encoding = Matrices::SymmetricMatrixEncoding::LowerPart )
-   {
-      if( isUndirected() && ! MatrixType::isSymmetric() ) {
-         std::map< std::pair< MapIndex, MapIndex >, MapValue > symmetric_map;
-         for( const auto& [ key, value ] : map ) {
-            symmetric_map[ { key.second, key.first } ] = value;
-            symmetric_map[ { key.first, key.second } ] = value;
-         }
-         this->adjacencyMatrix.setDimensions( nodesCount, nodesCount );
-         this->adjacencyMatrix.setElements( symmetric_map );
-      }
-      else {
-         this->adjacencyMatrix.setDimensions( nodesCount, nodesCount );
-         this->adjacencyMatrix.setElements( map, encoding );
-      }
-   }
+          Matrices::SymmetricMatrixEncoding encoding = Matrices::SymmetricMatrixEncoding::LowerPart );
 
+   //! \brief Copy-assignment operator.
    Graph&
    operator=( const Graph& ) = default;
 
+   //! \brief Move-assignment operator.
    Graph&
    operator=( Graph&& ) = default;
 
+   //! \brief Comparisons operator.
    bool
-   operator==( const Graph& other ) const
-   {
-      return adjacencyMatrix == other.adjacencyMatrix;
-   }
+   operator==( const Graph& other ) const;
 
+   //! \brief Sets the number of nodes in the graph.
    void
-   setNodeCount( IndexType nodesCount )
-   {
-      adjacencyMatrix.setDimensions( nodesCount, nodesCount );
-   }
+   setNodeCount( IndexType nodesCount );
 
+   /**
+    * \brief Sets the edges of the graph from a map.
+    *
+    * \tparam MapIndex is type for indexing of the nodes in the map.
+    * \tparam MapValue is type for weights of the edges in the map.
+    *
+    * \param map is the map with keys as (source node, target node) pairs and values as edge weights.
+    */
    template< typename MapIndex, typename MapValue >
    void
-   setEdges( const std::map< std::pair< MapIndex, MapIndex >, MapValue >& map )
-   {
-      this->adjacencyMatrix.setElements( map );
-   }
+   setEdges( const std::map< std::pair< MapIndex, MapIndex >, MapValue >& map );
 
+   //! \brief Returns the number of nodes in the graph.
    [[nodiscard]] IndexType
-   getNodeCount() const
-   {
-      return adjacencyMatrix.getRows();
-   }
+   getNodeCount() const;
 
+   //! \brief Returns the number of edges in the graph.
    [[nodiscard]] IndexType
-   getEdgeCount() const
-   {
-      if constexpr( isUndirected() )
-         return adjacencyMatrix.getNonzeroElementsCount() / 2;
-      return adjacencyMatrix.getNonzeroElementsCount();
-   }
+   getEdgeCount() const;
 
+   /**
+    * \brief Sets the capacities of the graph nodes.
+    *
+    * \param nodeCapacities is the vector holding the node capacities.
+    *
+    * The method sets the row capacities of the adjacency matrix to the provided node capacities.
+    * It is not necessary to call this method if the adjacency matrix is dense. If the adjacency
+    * matrix is sparse and symmetric, capacity only for edges in one direction (e.g., lower part)
+    * should be provided.
+    */
    template< typename Vector >
    void
-   setNodeCapacities( const Vector& nodeCapacities )
-   {
-      adjacencyMatrix.setRowCapacities( nodeCapacities );
-   }
+   setNodeCapacities( const Vector& nodeCapacities );
 
+   //! \brief Returns the constant adjacency matrix of the graph.
    [[nodiscard]] const MatrixType&
-   getAdjacencyMatrix() const
-   {
-      return adjacencyMatrix;
-   }
+   getAdjacencyMatrix() const;
 
+   //! \brief Returns the modifiable adjacency matrix of the graph.
    MatrixType&
-   getAdjacencyMatrix()
-   {
-      return adjacencyMatrix;
-   }
+   getAdjacencyMatrix();
 
+   /**
+    * \brief Sets the adjacency matrix of the graph.
+    *
+    * \tparam Matrix_ is type of the input adjacency matrix.
+    *
+    * \param matrix is the input adjacency matrix.
+    *
+    * The method makes a copy of the provided adjacency matrix and so
+    * the type if the input matrix can be different from the type of the
+    * adjacency matrix of the graph.
+    */
    template< typename Matrix_ >
    void
-   setAdjacencyMatrix( Matrix_ matrix )
-   {
-      if( matrix.getRows() != matrix.getColumns() )
-         throw std::logic_error( "Graph: adjacency matrix must be square matrix." );
-      adjacencyMatrix = std::move( matrix );
-   }
+   setAdjacencyMatrix( Matrix_ matrix );
 
-   [[nodiscard]] ValueType
-   getTotalWeight() const
-   {
-      auto values_view = adjacencyMatrix.getValues().getConstView();
-      auto column_indexes_view = adjacencyMatrix.getColumnIndexes().getConstView();
-      ValueType w = Algorithms::reduce< DeviceType >(
-         0,
-         values_view.getSize(),
-         [ = ] __cuda_callable__( IndexType i )
-         {
-            if( column_indexes_view[ i ] != Matrices::paddingIndex< IndexType > )
-               return values_view[ i ];
-            return (ValueType) 0;
-         },
-         TNL::Plus{} );
-      if constexpr( isUndirected() && ! MatrixType::isSymmetric() )
-         return 0.5 * w;
-      return w;
-   }
-
+   //! \brief Destructor.
    ~Graph() = default;
 
 protected:
    MatrixType adjacencyMatrix;
 };
 
+//! \brief Output stream operator for the \e Graph class.
 template< typename Matrix, GraphTypes GraphType >
 std::ostream&
-operator<<( std::ostream& os, const Graph< Matrix, GraphType >& graph )
-{
-   os << graph.getAdjacencyMatrix();
-   return os;
-}
+operator<<( std::ostream& os, const Graph< Matrix, GraphType >& graph );
 
 }  // namespace TNL::Graphs
+
+#include "Graph.hpp"
