38e88965d47687b47a7020634f5bc6bbc300e308
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2025-12-11 18:43:13 +0100

    Moving tests for sparse-dense matrix copying operations from SparseMatrixOperationsTest to a new test suite SparseDenseMatrixOperationsTest.

---
+++ src/UnitTests/Matrices/SparseDenseMatrixOperationsTest.hpp
@@ -0,0 +1,211 @@
+#pragma once
+
+#include <functional>
+#include <iostream>
+#include <sstream>
+
+#include <TNL/Matrices/SparseMatrix.h>
+#include <TNL/Containers/StaticVector.h>
+#include <TNL/Containers/Vector.h>
+#include <TNL/Containers/VectorView.h>
+
+#include <gtest/gtest.h>
+
+template< typename Matrix >
+void
+copyDenseToDenseMatrix_test()
+{
+   using RealType = typename Matrix::RealType;
+   using DeviceType = typename Matrix::DeviceType;
+   using IndexType = typename Matrix::IndexType;
+   using DenseMatrixType = TNL::Matrices::DenseMatrix< RealType, DeviceType, IndexType >;
+
+   // Create source matrix
+   // clang-format off
+   DenseMatrixType source{
+     { 1, 2, 3 },
+     { 4, 5, 6 },
+     { 7, 8, 9 } };
+   // clang-format on
+
+   // Create destination matrix
+   DenseMatrixType destination( 3, 3 );
+
+   // Copy matrix
+   TNL::Matrices::copyDenseToDenseMatrix( destination, source );
+
+   // Verify all elements
+   EXPECT_EQ( destination.getElement( 0, 0 ), 1.0 );
+   EXPECT_EQ( destination.getElement( 0, 1 ), 2.0 );
+   EXPECT_EQ( destination.getElement( 0, 2 ), 3.0 );
+   EXPECT_EQ( destination.getElement( 1, 0 ), 4.0 );
+   EXPECT_EQ( destination.getElement( 1, 1 ), 5.0 );
+   EXPECT_EQ( destination.getElement( 1, 2 ), 6.0 );
+   EXPECT_EQ( destination.getElement( 2, 0 ), 7.0 );
+   EXPECT_EQ( destination.getElement( 2, 1 ), 8.0 );
+   EXPECT_EQ( destination.getElement( 2, 2 ), 9.0 );
+
+   // Test with different sizes
+   // clang-format off
+   DenseMatrixType source2{
+      { 10.0, 20.0, 30.0, 40.0 },
+      { 50.0, 60.0, 70.0, 80.0 } };
+   // clang-format on
+
+   DenseMatrixType destination2( 2, 4 );
+   TNL::Matrices::copyDenseToDenseMatrix( destination2, source2 );
+
+   EXPECT_EQ( destination2.getElement( 0, 0 ), 10.0 );
+   EXPECT_EQ( destination2.getElement( 0, 1 ), 20.0 );
+   EXPECT_EQ( destination2.getElement( 0, 2 ), 30.0 );
+   EXPECT_EQ( destination2.getElement( 0, 3 ), 40.0 );
+   EXPECT_EQ( destination2.getElement( 1, 0 ), 50.0 );
+   EXPECT_EQ( destination2.getElement( 1, 1 ), 60.0 );
+   EXPECT_EQ( destination2.getElement( 1, 2 ), 70.0 );
+   EXPECT_EQ( destination2.getElement( 1, 3 ), 80.0 );
+}
+
+template< typename Matrix >
+void
+copySparseToDenseMatrix_test()
+{
+   using RealType = typename Matrix::RealType;
+   using DeviceType = typename Matrix::DeviceType;
+   using IndexType = typename Matrix::IndexType;
+   using DenseMatrixType = TNL::Matrices::DenseMatrix< RealType, DeviceType, IndexType >;
+
+   // Create sparse source matrix
+   // clang-format off
+  // /  1  2  0 |
+  // |  0  5  6 |
+  // \  7  0  9 /
+  Matrix source( 3, 3,
+  { { 0, 0, 1 }, { 0, 1, 2 },
+                 { 1, 1, 5 }, { 1, 2, 6 },
+    { 2, 0, 7 },                           { 2, 2, 9 } } );
+   // clang-format on
+
+   // Create dense destination matrix
+   DenseMatrixType destination( 3, 3 );
+
+   // Copy matrix
+   TNL::Matrices::copySparseToDenseMatrix( destination, source );
+
+   // Verify all elements
+   EXPECT_EQ( destination.getElement( 0, 0 ), 1.0 );
+   EXPECT_EQ( destination.getElement( 0, 1 ), 2.0 );
+   EXPECT_EQ( destination.getElement( 0, 2 ), 0.0 );
+   EXPECT_EQ( destination.getElement( 1, 0 ), 0.0 );
+   EXPECT_EQ( destination.getElement( 1, 1 ), 5.0 );
+   EXPECT_EQ( destination.getElement( 1, 2 ), 6.0 );
+   EXPECT_EQ( destination.getElement( 2, 0 ), 7.0 );
+   EXPECT_EQ( destination.getElement( 2, 1 ), 0.0 );
+   EXPECT_EQ( destination.getElement( 2, 2 ), 9.0 );
+
+   // Test with different sizes (non-square matrix)
+   // clang-format off
+  // / 10  0 20  0 |
+  // \  0 50  0 70 /
+  Matrix source2( 2, 4,
+  { { 0, 0, 10 },               { 0, 2, 20 },
+                  { 1, 1, 50 },               { 1, 3, 70 } } );
+   // clang-format on
+
+   DenseMatrixType destination2( 2, 4 );
+   TNL::Matrices::copySparseToDenseMatrix( destination2, source2 );
+
+   EXPECT_EQ( destination2.getElement( 0, 0 ), 10.0 );
+   EXPECT_EQ( destination2.getElement( 0, 1 ), 0.0 );
+   EXPECT_EQ( destination2.getElement( 0, 2 ), 20.0 );
+   EXPECT_EQ( destination2.getElement( 0, 3 ), 0.0 );
+   EXPECT_EQ( destination2.getElement( 1, 0 ), 0.0 );
+   EXPECT_EQ( destination2.getElement( 1, 1 ), 50.0 );
+   EXPECT_EQ( destination2.getElement( 1, 2 ), 0.0 );
+   EXPECT_EQ( destination2.getElement( 1, 3 ), 70.0 );
+
+   // Test with empty sparse matrix
+   Matrix source3( 2, 2 );
+   DenseMatrixType destination3( 2, 2 );
+   TNL::Matrices::copySparseToDenseMatrix( destination3, source3 );
+
+   EXPECT_EQ( destination3.getElement( 0, 0 ), 0.0 );
+   EXPECT_EQ( destination3.getElement( 0, 1 ), 0.0 );
+   EXPECT_EQ( destination3.getElement( 1, 0 ), 0.0 );
+   EXPECT_EQ( destination3.getElement( 1, 1 ), 0.0 );
+}
+
+template< typename Matrix >
+void
+copyDenseToSparseMatrix_test()
+{
+   using RealType = typename Matrix::RealType;
+   using DeviceType = typename Matrix::DeviceType;
+   using IndexType = typename Matrix::IndexType;
+   using DenseMatrixType = TNL::Matrices::DenseMatrix< RealType, DeviceType, IndexType >;
+
+   // Create dense source matrix
+   // clang-format off
+  DenseMatrixType source{
+    { 1, 2, 0 },
+    { 0, 5, 6 },
+    { 7, 0, 9 } };
+   // clang-format on
+
+   // Create sparse destination matrix
+   Matrix destination( 3, 3 );
+
+   // Copy matrix
+   TNL::Matrices::copyDenseToSparseMatrix( destination, source );
+
+   // Verify all elements
+   EXPECT_EQ( destination.getElement( 0, 0 ), 1.0 );
+   EXPECT_EQ( destination.getElement( 0, 1 ), 2.0 );
+   EXPECT_EQ( destination.getElement( 0, 2 ), 0.0 );
+   EXPECT_EQ( destination.getElement( 1, 0 ), 0.0 );
+   EXPECT_EQ( destination.getElement( 1, 1 ), 5.0 );
+   EXPECT_EQ( destination.getElement( 1, 2 ), 6.0 );
+   EXPECT_EQ( destination.getElement( 2, 0 ), 7.0 );
+   EXPECT_EQ( destination.getElement( 2, 1 ), 0.0 );
+   EXPECT_EQ( destination.getElement( 2, 2 ), 9.0 );
+
+   // Test with different sizes (non-square matrix)
+   // clang-format off
+  DenseMatrixType source2{
+    { 10,  0, 20,  0 },
+    {  0, 50,  0, 70 } };
+   // clang-format on
+
+   Matrix destination2( 2, 4 );
+   TNL::Matrices::copyDenseToSparseMatrix( destination2, source2 );
+
+   EXPECT_EQ( destination2.getElement( 0, 0 ), 10.0 );
+   EXPECT_EQ( destination2.getElement( 0, 1 ), 0.0 );
+   EXPECT_EQ( destination2.getElement( 0, 2 ), 20.0 );
+   EXPECT_EQ( destination2.getElement( 0, 3 ), 0.0 );
+   EXPECT_EQ( destination2.getElement( 1, 0 ), 0.0 );
+   EXPECT_EQ( destination2.getElement( 1, 1 ), 50.0 );
+   EXPECT_EQ( destination2.getElement( 1, 2 ), 0.0 );
+   EXPECT_EQ( destination2.getElement( 1, 3 ), 70.0 );
+
+   // Test with all zeros matrix
+   DenseMatrixType source3{ { 0, 0 }, { 0, 0 } };
+
+   Matrix destination3( 2, 2 );
+   TNL::Matrices::copyDenseToSparseMatrix( destination3, source3 );
+
+   EXPECT_EQ( destination3.getElement( 0, 0 ), 0.0 );
+   EXPECT_EQ( destination3.getElement( 0, 1 ), 0.0 );
+   EXPECT_EQ( destination3.getElement( 1, 0 ), 0.0 );
+   EXPECT_EQ( destination3.getElement( 1, 1 ), 0.0 );
+
+   // Test with full dense matrix
+   DenseMatrixType source4{ { 1, 2 }, { 3, 4 } };
+
+   Matrix destination4( 2, 2 );
+   TNL::Matrices::copyDenseToSparseMatrix( destination4, source4 );
+
+   EXPECT_EQ( destination4.getElement( 0, 0 ), 1.0 );
+   EXPECT_EQ( destination4.getElement( 0, 1 ), 2.0 );
+   EXPECT_EQ( destination4.getElement( 1, 0 ), 3.0 );
+   EXPECT_EQ( destination4.getElement( 1, 1 ), 4.0 );
+}
---
+++ src/UnitTests/Matrices/SparseDenseMatrixOperationsTest_CSR.h
@@ -0,0 +1,65 @@
+#include <TNL/Algorithms/Segments/CSR.h>
+#include <TNL/Matrices/SparseMatrix.h>
+#include <TNL/Arithmetics/Complex.h>
+
+#include <gtest/gtest.h>
+
+// types for which SparseMatrixOperationsTest is instantiated
+using MatrixTypes = ::testing::Types<
+   TNL::Matrices::SparseMatrix< int, TNL::Devices::Host, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< long, TNL::Devices::Host, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< float, TNL::Devices::Host, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< double, TNL::Devices::Host, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< int, TNL::Devices::Host, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< long, TNL::Devices::Host, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< float, TNL::Devices::Host, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::
+      SparseMatrix< double, TNL::Devices::Host, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR > /*,
+TNL::Matrices::SparseMatrix< std::complex< float >,
+                     TNL::Devices::Host,
+                     long,
+                     TNL::Matrices::GeneralMatrix,
+                     TNL::Algorithms::Segments::CSR >*/
+#if defined( __CUDACC__ )
+   ,
+   TNL::Matrices::SparseMatrix< int, TNL::Devices::Cuda, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   //TNL::Matrices::SparseMatrix< long, TNL::Devices::Cuda, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   // TODO: This is not accepted by nvcc 13.0 -- long is somehow transformed to TNL::Config::Integer
+   // src/TNL/Matrices/SparseMatrixBase.h(45): error: static assertion failed with "Given Real type is not supported by atomic
+   // operations on GPU which are necessary for symmetric operations." static_assert( detected during:
+   //    instantiation of class "TNL::Matrices::SparseMatrixBase<Real, Device, Index, MatrixType, SegmentsView, ComputeReal>
+   //    [with Real=TNL::Config::Integer, ..."
+   TNL::Matrices::SparseMatrix< float, TNL::Devices::Cuda, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< double, TNL::Devices::Cuda, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< int, TNL::Devices::Cuda, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   //TNL::Matrices::SparseMatrix< long, TNL::Devices::Cuda, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR>,
+   // TODO: The same issue as above
+   TNL::Matrices::SparseMatrix< float, TNL::Devices::Cuda, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::
+      SparseMatrix< double, TNL::Devices::Cuda, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR > /*,
+TNL::Matrices::SparseMatrix< TNL::Arithmetics::Complex< float >,
+                  TNL::Devices::Cuda,
+                  long,
+                  TNL::Matrices::GeneralMatrix,
+                  TNL::Algorithms::Segments::CSR >*/
+#elif defined( __HIP__ )
+   ,
+   TNL::Matrices::SparseMatrix< int, TNL::Devices::Hip, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< long, TNL::Devices::Hip, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< float, TNL::Devices::Hip, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< double, TNL::Devices::Hip, int, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< int, TNL::Devices::Hip, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< long, TNL::Devices::Hip, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::SparseMatrix< float, TNL::Devices::Hip, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR >,
+   TNL::Matrices::
+      SparseMatrix< double, TNL::Devices::Hip, long, TNL::Matrices::GeneralMatrix, TNL::Algorithms::Segments::CSR > /*,
+TNL::Matrices::SparseMatrix< TNL::Arithmetics::Complex< float >,
+                     TNL::Devices::Hip,
+                     long,
+                     TNL::Matrices::GeneralMatrix,
+                     TNL::Algorithms::Segments::CSR >*/
+#endif
+   >;
+
+#include "SparseDenseMatrixOperationsTest.h"
+#include "../main.h"
