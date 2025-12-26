4cdff761debf68a33b23f2318e0df324af44674b
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2025-12-26 17:39:29 +0100

    Removing segments functions for finding, sorting and scan having segment indexes range parameters.

--- src/TNL/Algorithms/Segments/find.hpp
+++ src/TNL/Algorithms/Segments/find.hpp
@@ -8,6 +8,18 @@
 
 namespace TNL::Algorithms::Segments {
 
+template< typename Segments, typename Condition, typename ResultKeeper >
+static void
+findInAllSegments( const Segments& segments, Condition&& condition, ResultKeeper&& keeper, LaunchConfiguration launchConfig )
+{
+   findInSegments( segments,
+                   (typename Segments::IndexType) 0,
+                   segments.getSegmentsCount(),
+                   std::forward< Condition >( condition ),
+                   std::forward< ResultKeeper >( keeper ),
+                   launchConfig );
+}
+
 template< typename Segments, typename IndexBegin, typename IndexEnd, typename Condition, typename ResultKeeper, typename T >
 static void
 findInSegments( const Segments& segments,
@@ -26,42 +38,6 @@ findInSegments( const Segments& segments,
                                launchConfig );
 }
 
-template< typename Segments, typename Condition, typename ResultKeeper >
-static void
-findInAllSegments( const Segments& segments, Condition&& condition, ResultKeeper&& keeper, LaunchConfiguration launchConfig )
-{
-   findInSegments( segments,
-                   (typename Segments::IndexType) 0,
-                   segments.getSegmentsCount(),
-                   std::forward< Condition >( condition ),
-                   std::forward< ResultKeeper >( keeper ),
-                   launchConfig );
-}
-
-template< typename Segments,
-          typename Array,
-          typename IndexBegin,
-          typename IndexEnd,
-          typename Condition,
-          typename ResultKeeper,
-          typename T >
-static void
-findInSegments( const Segments& segments,
-                const Array& segmentIndexes,
-                IndexBegin begin,
-                IndexEnd end,
-                Condition&& condition,
-                ResultKeeper&& keeper,
-                LaunchConfiguration launchConfig )
-{
-   reduceSegmentsWithArgument( segments,
-                               segmentIndexes.getConstView( begin, end ),
-                               std::forward< Condition >( condition ),
-                               AnyWithArg{},
-                               std::forward< ResultKeeper >( keeper ),
-                               launchConfig );
-}
-
 template< typename Segments, typename Array, typename Condition, typename ResultKeeper, typename T >
 static void
 findInSegments( const Segments& segments,
@@ -70,13 +46,29 @@ findInSegments( const Segments& segments,
                 ResultKeeper&& keeper,
                 LaunchConfiguration launchConfig )
 {
-   findInSegments( segments,
-                   segmentIndexes,
-                   (typename Segments::IndexType) 0,
-                   segmentIndexes.getSize(),
-                   std::forward< Condition >( condition ),
-                   std::forward< ResultKeeper >( keeper ),
-                   launchConfig );
+   reduceSegmentsWithArgument( segments,
+                               segmentIndexes,
+                               std::forward< Condition >( condition ),
+                               AnyWithArg{},
+                               std::forward< ResultKeeper >( keeper ),
+                               launchConfig );
+}
+
+template< typename Segments, typename SegmentCondition, typename Condition, typename ResultKeeper >
+static void
+findInAllSegmentsIf( const Segments& segments,
+                     SegmentCondition&& segmentCondition,
+                     Condition&& condition,
+                     ResultKeeper&& keeper,
+                     LaunchConfiguration launchConfig )
+{
+   findInSegmentsIf( segments,
+                     (typename Segments::IndexType) 0,
+                     segments.getSegmentsCount(),
+                     std::forward< SegmentCondition >( segmentCondition ),
+                     std::forward< Condition >( condition ),
+                     std::forward< ResultKeeper >( keeper ),
+                     launchConfig );
 }
 
 template< typename Segments,
@@ -110,23 +102,6 @@ findInSegmentsIf( const Segments& segments,
                                  AnyWithArg{},
                                  keep_,
                                  launchConfig );
-}
-
-template< typename Segments, typename SegmentCondition, typename Condition, typename ResultKeeper >
-static void
-findInSegmentsIf( const Segments& segments,
-                  SegmentCondition&& segmentCondition,
-                  Condition&& condition,
-                  ResultKeeper&& keeper,
-                  LaunchConfiguration launchConfig )
-{
-   findInSegmentsIf( segments,
-                     (typename Segments::IndexType) 0,
-                     segments.getSegmentsCount(),
-                     std::forward< SegmentCondition >( segmentCondition ),
-                     std::forward< Condition >( condition ),
-                     std::forward< ResultKeeper >( keeper ),
-                     launchConfig );
 }
 
 }  // namespace TNL::Algorithms::Segments
