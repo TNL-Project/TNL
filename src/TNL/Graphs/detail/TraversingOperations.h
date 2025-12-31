06118e175e16d07cfec3373e2fe7d8f72d722376
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2025-12-31 14:20:49 +0100

    Added traversing functions for graphs.

---
+++ src/TNL/Graphs/detail/TraversingOperations.h
@@ -0,0 +1,14 @@
+// SPDX-FileComment: This file is part of TNL - Template Numerical Library (https://tnl-project.org/)
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+namespace TNL::Graphs::detail {
+
+template< typename Graph >
+struct TraversingOperations
+{};
+
+}  //namespace TNL::Graphs::detail
+
+#include "TraversingOperations_GraphView.h"
