46fda5e9086751af0f7798890040e3aa5bdbeff1
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2026-01-06 14:54:08 +0100

    Added documentation to graph readers and writers.

--- src/TNL/Graphs/Readers/MtxReader.h
+++ src/TNL/Graphs/Readers/MtxReader.h
@@ -9,6 +9,14 @@
 
 namespace TNL::Graphs::Readers {
 
+/**
+ * \brief Reader for graphs in Matrix Market format.
+ *
+ * This reader uses \ref TNL::Matrices::MatrixReader to read the adjacency matrix of the graph
+ * from a file in Matrix Market format (.mtx) and sets it as the adjacency matrix of the provided graph.
+ *
+ * \tparam Graph The type of the graph to be read.
+ */
 template< typename Graph >
 struct MtxReader
 {
@@ -17,6 +25,12 @@ struct MtxReader
    using IndexType = typename Graph::IndexType;
    using MatrixType = typename Graph::AdjacencyMatrixType;
 
+   /**
+    * \brief Reads a graph from a Matrix Market file.
+    *
+    * \param file_name is the name of the file containing the adjacency matrix in Matrix Market format.
+    * \param graph is the graph object to be populated.
+    */
    static void
    read( const std::string& file_name, Graph& graph )
    {
