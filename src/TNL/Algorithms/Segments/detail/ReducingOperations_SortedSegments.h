fedb52926f594a70ad26733738085591f48af039
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2026-01-15 20:51:27 +0100

    Updating copyright headers.

--- src/TNL/Algorithms/Segments/detail/ReducingOperations_SortedSegments.h
+++ src/TNL/Algorithms/Segments/detail/ReducingOperations_SortedSegments.h
@@ -1,5 +1,5 @@
-// SPDX-FileComment: This file is part of TNL - Template Numerical Library
-// (https://tnl-project.org/) SPDX-License-Identifier: MIT
+// SPDX-FileComment: This file is part of TNL - Template Numerical Library (https://tnl-project.org/)
+// SPDX-License-Identifier: MIT
 
 #pragma once
 
