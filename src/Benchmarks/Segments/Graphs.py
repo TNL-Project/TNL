b581237bbd5ec1eb1437c9b14232f45f4b5006d1
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2026-01-15 20:51:27 +0100

    Updating copyright headers.

--- src/Benchmarks/Segments/Graphs.py
+++ src/Benchmarks/Segments/Graphs.py
@@ -1,3 +1,6 @@
+# SPDX-FileComment: This file is part of TNL - Template Numerical Library (https://tnl-project.org/)
+# SPDX-License-Identifier: MIT
+
 import matplotlib.pyplot as plt
 import numpy as np
 
