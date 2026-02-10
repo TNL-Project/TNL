53f08e1ef1c2226e2175a2d4f66425a2c1378fd3
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2026-02-10 20:11:17 +0100

    Refactoring PDLP solver.

--- src/TNL/Solvers/Optimization/KKTData.h
+++ src/TNL/Solvers/Optimization/KKTData.h
@@ -13,6 +13,15 @@ struct KKTData
    Real primal_objective;
    Real dual_objective;
 
+   KKTData() = default;
+
+   KKTData( Real primal_feasibility, Real dual_feasibility, Real primal_objective, Real dual_objective )
+   : primal_feasibility( primal_feasibility ),
+     dual_feasibility( dual_feasibility ),
+     primal_objective( primal_objective ),
+     dual_objective( dual_objective )
+   {}
+
    Real
    getPrimalFeasibility() const
    {
@@ -44,10 +53,27 @@ struct KKTData
    }
 
    Real
-   getKKTError( const Real& omega ) const;
+   getKKTError( const Real& omega ) const
+   {
+      const Real omega_sqr = omega * omega;
+      const Real error =
+         sqrt( omega_sqr * primal_feasibility * primal_feasibility + 1.0 / omega_sqr * ( dual_feasibility * dual_feasibility )
+               + pow( primal_objective - dual_objective, 2 ) );
+
+#ifdef PRINTING
+      std::cout << " omega sqr. = " << omega_sqr << " primal feas. = " << primal_feasibility
+                << " dual feas. = " << dual_feasibility << " primal obj. = " << primal_objective
+                << " dual obj. = " << dual_objective << " error = " << error << std::endl;
+#endif
+      return error;
+   }
 
    Real
-   getRelativeDualityGap() const;
+   getRelativeDualityGap() const
+   {
+      return std::abs( ( primal_objective - dual_objective )
+                       / ( (Real) 1.0 + std::abs( primal_objective ) + std::abs( dual_objective ) ) );
+   }
 };
 
 }  // namespace TNL::Solvers::Optimization
