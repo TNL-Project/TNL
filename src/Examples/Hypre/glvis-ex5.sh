0c067a2aff3a7a3bd5cb10ce018bdcd96fb6dde5
Jakub Klinkovský <jakub.klinkovsky@fjfi.cvut.cz>
2026-03-13 12:00:18 +0100

    Fix shellcheck linting

--- src/Examples/Hypre/glvis-ex5.sh
+++ src/Examples/Hypre/glvis-ex5.sh
@@ -12,11 +12,13 @@ if ! test -e "$mesh"; then
     exit 1
 fi
 
-echo "FiniteElementSpace" > "$sol"
-echo "FiniteElementCollection: H1_2D_P1" >> "$sol"
-echo "VDim: 1" >> "$sol"
-echo "Ordering: 0" >> "$sol"
-echo "" >> "$sol"
-find "$dir" -name "$ex.sol.??????" | sort | xargs cat >> "$sol"
+{
+    echo "FiniteElementSpace"
+    echo "FiniteElementCollection: H1_2D_P1"
+    echo "VDim: 1"
+    echo "Ordering: 0"
+    echo ""
+    find "$dir" -name "$ex.sol.??????" | sort | xargs cat
+} > "$sol"
 
 glvis -m "$mesh" -g "$sol" -k "$keys"
