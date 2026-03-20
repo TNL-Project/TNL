7ee65a847d9a6dd3fcaafc60d45bf234562d5daa
Jakub Klinkovský <jakub.klinkovsky@fjfi.cvut.cz>
2026-03-20 14:15:26 +0100

    Refactor sorting benchmark to use TNL benchmark interface and modern C++
    - Remove using namespace std from tnl-benchmark-sort.h
    - Replace C-style function pointers with std::function for generator
      dispatch
    - Convert generator functions to templates with explicit type parameters
    - Initialize min variable in generateStaggered with ternary operator
    - Rename generateZero_entropy to generateZeroEntropy for consistency
    - Update benchmark interface to use TNL::Benchmarks::Benchmark and
      TNL::Config::ParameterContainer
    - Keep verification logic and throw runtime_error if any algorithm fails
      to produce ascending order

--- src/Benchmarks/Sorting/ReferenceAlgorithms/ThrustRadixsort.h
+++ src/Benchmarks/Sorting/ReferenceAlgorithms/ThrustRadixsort.h
@@ -4,9 +4,11 @@
 
 namespace TNL {
 
+template< typename ValueType >
 struct ThrustRadixsort
 {
-   static void sort( Containers::ArrayView< int, Devices::Cuda >& view )
+   static void
+   sort( Containers::ArrayView< ValueType, Devices::Cuda >& view )
    {
       //thrust::sort(thrust::device, view.getData(), view.getData() + view.getSize());
       //cudaDeviceSynchronize();
