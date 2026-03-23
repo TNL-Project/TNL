0f4a2d65f3c950377283b20eec32b147f6199473
Jakub Klinkovský <jakub.klinkovsky@fjfi.cvut.cz>
2026-03-23 22:36:24 +0100

    Replace ThrustRadixsort wrapper with CUBRadixSort
    - CUB is more low-level than Thrust and sufficient for our use case
    - Add new wrapper directly in the TNL::Algorithms namespace
    - Add documentation also for CUBMergeSort

--- src/TNL/Algorithms/Sorting/CUBMergeSort.h
+++ src/TNL/Algorithms/Sorting/CUBMergeSort.h
@@ -18,27 +18,58 @@
 
 namespace TNL::Algorithms::Sorting {
 
+/**
+ * \brief CUDA merge sort using CUB's DeviceMergeSort.
+ *
+ * This class provides a wrapper for merge sort implementation from NVIDIA's CUB library.
+ * It uses the `cub::DeviceMergeSort::SortKeys` algorithm which performs a merge sort
+ * on device-resident data.
+ *
+ * Merge sort differs from radix sort in several key aspects:
+ * - Can handle arbitrary types (as long as they are *LessThan Comparable*) and custom
+ *   comparison functors
+ * - Is not guaranteed to be stable
+ * - Typically slower than DeviceRadixSort for arithmetic types
+ *
+ * \par Example
+ * \include Algorithms/SortingCUBMergeSort.cu
+ * \par Output
+ * \include SortingCUBMergeSort.out
+ *
+ * \see CUBRadixSort for a faster radix sort alternative (for arithmetic types)
+ * \see https://nvidia.github.io/cccl/unstable/cub/api/structcub_1_1DeviceMergeSort.html
+ */
 struct CUBMergeSort
 {
+   /**
+    * \brief Sort array in ascending order.
+    *
+    * \tparam Array is a type of container to be sorted. It must be
+    *         \ref TNL::Containers::Array or \ref TNL::Containers::ArrayView with
+    *         \ref TNL::Devices::Cuda device type.
+    * \param array The array to sort (will be modified in-place).
+    */
    template< typename Array >
    static void
    sort( Array& array )
    {
-      using Value = typename Array::ValueType;
-      sort(
-         array,
-         [] __cuda_callable__( const Value& a, const Value& b )
-         {
-            return a < b;
-         } );
+      sort( array, std::less<>{} );
    }
 
+   /**
+    * \brief Sort array using custom comparison function.
+    *
+    * \param array The array to sort (will be modified in-place).
+    * \param compare Comparison function object that returns true if the first
+    *        argument should be ordered before the second.
+    */
    template< typename Array, typename Compare >
    static void
    sort( Array& array, const Compare& compare )
    {
       using Device = typename Array::DeviceType;
       static_assert( std::is_same_v< Device, Devices::Cuda >, "CUBMergeSort: unsupported device type" );
+
 #if defined( __CUDACC__ )
       const auto size = array.getSize();
       if( size <= 1 )
