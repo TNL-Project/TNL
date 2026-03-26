99e425539a7625b5b75bfce2a5e34c4ec93df24a
Tomas Oberhuber <oberhuber.tomas@gmail.com>
2026-03-26 10:23:23 +0100

    Deleting CUDA atomicAdd for double - it is not needed anymore.

--- src/TNL/Atomic.h
+++ src/TNL/Atomic.h
@@ -13,27 +13,6 @@
 #if defined( __CUDACC__ ) || defined( __HIP__ )
 namespace {
 
-// double-precision atomicAdd function for Maxwell and older GPUs
-// copied from: https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#atomic-functions
-   #if defined( __CUDA_ARCH__ ) && __CUDA_ARCH__ < 600
-__device__
-double
-atomicAdd( double* address, double val )
-{
-   unsigned long long int* address_as_ull = (unsigned long long int*) address;
-   unsigned long long int old = *address_as_ull, assumed;
-
-   do {
-      assumed = old;
-      old = atomicCAS( address_as_ull, assumed, __double_as_longlong( val + __longlong_as_double( assumed ) ) );
-
-      // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
-   } while( assumed != old );
-
-   return __longlong_as_double( old );
-}
-   #endif
-
 __device__
 inline long int
 atomicAdd( long int* address, long int val )
